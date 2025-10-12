872236a569a89361ca284c90fab9d828780b19cc
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-12 12:12:51 +0100

    Time: 99 ms (90.13%) | Memory: 18.2 MB (16.00%) - LeetSync

--- 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
@@ -1,24 +1,24 @@
 class Solution:
     def balancedString(self, s: str) -> int:
-        n = len(s)
-        k = n // 4
-        min_len = float('inf')
-        freq = Counter(s)
-        excess = {num: count-k for num, count in freq.items() if count > k}
-        if not excess:
+        max_len = len(s) // 4
+        freq = collections.Counter(s)
+        overflow = {num: count-max_len for num, count in freq.items() if count > max_len}
+        if not overflow:
             return 0
-        window = defaultdict(int)
-        have, need = 0, len(excess)
+        # now we hunt for the smallest continuous substring that countains all the elements in the overflow
+        # its length will resemble the size of the smallest substring that must be replaced to acheive balance
+        window = collections.Counter()
+        found, required = 0, len(overflow)
+        min_len = float('inf')
         l = 0
-        for r in range(n):
-            window[s[r]] += 1
-            if s[r] in excess and window[s[r]] == excess[s[r]]:
-                have += 1
-            while have == need:
-                if r - l + 1 < min_len:
-                    min_len = r - l + 1
-                if s[l] in excess and window[s[l]] == excess[s[l]]:
-                    have -= 1
+        for r, char in enumerate(s):
+            window[char] += 1
+            if char in overflow and window[char] == overflow[char]:
+                found += 1
+            while required == found:
+                min_len = min(min_len, r - l + 1)
+                if s[l] in overflow and window[s[l]] == overflow[s[l]]:
+                    found -= 1
                 window[s[l]] -= 1
                 l += 1
         return min_len
