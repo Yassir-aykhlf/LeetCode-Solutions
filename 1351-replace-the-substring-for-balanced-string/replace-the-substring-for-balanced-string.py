e37e5c0301529bd3bf12d75a2728f8def10585bb
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-15 19:07:14 +0100

    Time: 103 ms (86.87%) | Memory: 18.1 MB (49.44%) - LeetSync

--- 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
@@ -1,24 +1,23 @@
 class Solution:
     def balancedString(self, s: str) -> int:
-        max_len = len(s) // 4
+        n = len(s)
+        k = n // 4
         freq = collections.Counter(s)
-        overflow = {num: count-max_len for num, count in freq.items() if count > max_len}
+        overflow = {char: count-k for char, count in freq.items() if count > k}
         if not overflow:
             return 0
-        # now we hunt for the smallest continuous substring that countains all the elements in the overflow
-        # its length will resemble the size of the smallest substring that must be replaced to acheive balance
+        have, need = 0, len(overflow)
         window = collections.Counter()
-        found, required = 0, len(overflow)
         min_len = float('inf')
         l = 0
-        for r, char in enumerate(s):
-            window[char] += 1
-            if char in overflow and window[char] == overflow[char]:
-                found += 1
-            while required == found:
+        for r in range(n):
+            window[s[r]] += 1
+            if s[r] in overflow and window[s[r]] == overflow[s[r]]:
+                have += 1
+            while have == need:
                 min_len = min(min_len, r - l + 1)
                 if s[l] in overflow and window[s[l]] == overflow[s[l]]:
-                    found -= 1
+                    have -= 1
                 window[s[l]] -= 1
                 l += 1
-        return min_len
+        return min_len if min_len != float('inf') else 0
