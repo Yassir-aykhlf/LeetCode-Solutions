7db9d6f5452ce439e70e17379f60271af497c50a
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-16 17:52:49 +0100

    Time: 16 ms (27.91%) | Memory: 20.3 MB (55.63%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -7,8 +7,7 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
-
-        def mergeTwoList(l1, l2):
+        def mergeTwo(l1, l2):
             dummy = ListNode(-1)
             cur = dummy
             while l1 and l2:
@@ -21,12 +20,11 @@ class Solution:
                 cur = cur.next
             cur.next = l1 or l2
             return dummy.next
-
         while len(lists) > 1:
             merged = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i + 1] if i + 1 < len(lists) else None
-                merged.append(mergeTwoList(l1, l2))
+                merged.append(mergeTwo(l1,l2))
             lists = merged
-        return lists[0] if lists else None
+        return lists[0]
