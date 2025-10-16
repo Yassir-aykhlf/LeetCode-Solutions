56a445e6bdf6770fcdf2d3786b00780db70b9b4b
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-16 17:26:06 +0100

    Time: 0 ms (100.00%) | Memory: 18.9 MB (74.32%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,16 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-
-        write = 1
-        # start for index 1 because the first is always unique
-        for read in range(1, len(nums)):
-            # if we the current number is unique, write it
-            if nums[read] != nums[read - 1]:
-                nums[write] = nums[read]
-                write += 1
-            # else keep searching for the next unique item
-        # return the index after the last unique item,
-        # which is also the length of the unique string
-        return write
+        w = 0
+        for r in range(len(nums)):
+            if nums[r] != nums[w]:
+                w += 1
+                nums[w] = nums[r]
+        return w+1
