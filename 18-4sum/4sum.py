403eef93cea92c6fae0988ff6fa545c565b81241
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-16 18:54:21 +0100

    Time: 395 ms (50.20%) | Memory: 17.7 MB (96.41%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,28 +1,28 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        nums.sort()
         n = len(nums)
+        nums.sort()
         result = []
         for i in range(n - 3):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            for j in range(i + 1, n - 2):
+            for j in range(i+1, n - 2):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
-                a = j + 1
-                b = n - 1
-                while a < b:
-                    comb = nums[i] + nums[j] + nums[a] + nums[b]
+                l = j + 1
+                r = n - 1
+                while l < r:
+                    comb = nums[i] + nums[j] + nums[l] + nums[r]
                     if comb == target:
-                        result.append([nums[i], nums[j], nums[a], nums[b]])
-                        while a < b and nums[a] == nums[a + 1]:
-                            a += 1
-                        while a < b and nums[b] == nums[b - 1]:
-                            b -= 1
-                        a += 1
-                        b -= 1
+                        result.append([nums[i], nums[j], nums[l], nums[r]])
+                        while l < r and nums[l] == nums[l+1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r-1]:
+                            r -= 1
+                        l += 1
+                        r -= 1
                     elif comb < target:
-                        a += 1
+                        l += 1
                     else:
-                        b -= 1
+                        r -= 1
         return result
