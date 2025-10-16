2d34663f9470c316ee281d2bc10f53e0df73e870
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-16 16:45:26 +0100

    Time: 291 ms (5.43%) | Memory: 23.6 MB (39.54%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,15 +1,14 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        window = collections.defaultdict(int)
         max_len = 0
-        n = len(fruits)
+        window = collections.Counter()
         l = 0
-        for r in range(n):
+        for r in range(len(fruits)):
             window[fruits[r]] += 1
             while len(window) > 2:
                 window[fruits[l]] -= 1
                 if window[fruits[l]] == 0:
-                    del window[fruits[l]] # because we rely on the size of dict
+                    del window[fruits[l]]
                 l += 1
             max_len = max(max_len, r - l + 1)
         return max_len
