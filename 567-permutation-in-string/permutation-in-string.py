872dabf53d9a4ff0c9990effb2cdc379426324bb
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-16 16:35:31 +0100

    Time: 55 ms (33.04%) | Memory: 18.1 MB (19.23%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,16 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        pattern = collections.Counter(s1)
+        target = collections.Counter(s1)
         k = len(s1)
-        window = s2[:k]
-        window_state = collections.Counter(window)
-        if window_state == pattern:
+        n = len(s2)
+        window = collections.Counter(s2[:k])
+        if window == target:
             return True
-        for r in range(k, len(s2)):
-            window_state[s2[r]] += 1
-            window_state[s2[r - k]] -= 1
-            if window_state[s2[r - k]] == 0:
-                del window_state[s2[r - k]]
-            if window_state == pattern:
+        for r in range(k, n):
+            window[s2[r]] += 1
+            window[s2[r - k]] -= 1
+            if window[s2[r - k]] == 0:
+                del window[s2[r - k]]
+            if window == target:
                 return True
         return False
