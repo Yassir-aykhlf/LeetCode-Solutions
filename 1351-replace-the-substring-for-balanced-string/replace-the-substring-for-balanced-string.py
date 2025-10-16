e1a672dd5e8f30c8fd7f675c706ff866fdc2ba62
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-16 10:40:36 +0100

    Time: 99 ms (89.30%) | Memory: 18.1 MB (49.01%) - LeetSync

--- 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
@@ -3,21 +3,22 @@ class Solution:
         n = len(s)
         k = n // 4
         freq = collections.Counter(s)
-        overflow = {char: count-k for char, count in freq.items() if count > k}
-        if not overflow:
+        excess = {char: count-k for char, count in freq.items() if count > k}
+        if not excess:
             return 0
-        have, need = 0, len(overflow)
         window = collections.Counter()
+        have = 0
+        need = len(excess)
         min_len = float('inf')
         l = 0
-        for r in range(n):
+        for r in range(len(s)):
             window[s[r]] += 1
-            if s[r] in overflow and window[s[r]] == overflow[s[r]]:
+            if s[r] in excess and window[s[r]] == excess[s[r]]:
                 have += 1
             while have == need:
                 min_len = min(min_len, r - l + 1)
-                if s[l] in overflow and window[s[l]] == overflow[s[l]]:
-                    have -= 1
+                if s[l] in excess and window[s[l]] == excess[s[l]]:
+                    have -= 1 
                 window[s[l]] -= 1
                 l += 1
         return min_len if min_len != float('inf') else 0
