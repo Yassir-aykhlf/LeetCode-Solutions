84d6cff49d5889e09fa943878a644bff9607ec6f
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-17 09:59:59 +0100

    Time: 12 ms (45.62%) | Memory: 20.3 MB (55.67%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -24,7 +24,7 @@ class Solution:
             merged = []
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
-                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                l2 = lists[i + 1] if i+1 < len(lists) else None
                 merged.append(mergeTwo(l1, l2))
             lists = merged
         return lists[0]
