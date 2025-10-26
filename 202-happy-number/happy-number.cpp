41b92ec1bd1177cc01091ae317048e910cf97570
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-26 17:23:05 +0100

    Time: 7 ms (3.50%) | Memory: 7.8 MB (85.88%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int calcSum(int n) {
+        int res = 0;
+        while (n) {
+            res += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return res;
+    }
+public:
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        while (true) {
+            slow = calcSum(slow);
+            fast = calcSum(calcSum(fast));
+                std::cout << slow << " ";
+            if (slow == fast) {
+                return slow == 1;
+            }
+        }
+    }
+};
