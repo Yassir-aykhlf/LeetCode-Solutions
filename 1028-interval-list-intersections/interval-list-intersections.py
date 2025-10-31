fbc67d33577b65ef46538f8d4172f68284988e84
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-10-31 09:48:45 +0100

    Time: 7 ms (50.09%) | Memory: 18.6 MB (68.63%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.py
+++ 1028-interval-list-intersections/interval-list-intersections.py
@@ -6,10 +6,9 @@ class Solution:
         while p1 < len(l1) and p2 < len(l2):
             s1, e1 = l1[p1]
             s2, e2 = l2[p2]
-            l_inter = max(s1, s2)
-            r_inter = min(e1, e2)
-            if l_inter <= r_inter:
-                res.append([l_inter, r_inter])
+            inter_l, inter_r = max(s1, s2), min(e1, e2)
+            if inter_l <= inter_r:
+                res.append([inter_l, inter_r])
             if e1 < e2:
                 p1 += 1
             else:
