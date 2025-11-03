68fcac31f8111bc59d6f058dc315269770133f48
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-03 10:53:04 +0100

    Time: 0 ms (100.00%) | Memory: 17.9 MB (56.26%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -1,11 +1,7 @@
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
-        freq = {}
+        freq = collections.Counter(arr)
         largest = -1
-        for num in arr:
-            if num not in freq:
-                freq[num] = 0
-            freq[num] += 1
         for num in arr:
             if freq[num] == num:
                 largest = max(largest, num)
