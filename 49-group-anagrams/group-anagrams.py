4195f42e088ef996284b52552e4642cb0e858545
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-05 12:00:49 +0100

    Time: 12 ms (60.38%) | Memory: 20.6 MB (75.64%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,6 +2,6 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         group = collections.defaultdict(list)
         for str_ in strs:
-            code = ''.join(sorted(str_))
-            group[code].append(str_)
-        return [arr for arr in group.values()]
+            key = ''.join(sorted(str_))
+            group[key].append(str_)
+        return [lst for lst in group.values()]
