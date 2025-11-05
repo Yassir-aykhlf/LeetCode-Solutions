feca40e83561dea89d781a16b164cddbd52fca56
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-05 11:58:02 +0100

    Time: 75 ms (95.81%) | Memory: 18.2 MB (13.47%) - LeetSync

--- 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
@@ -2,22 +2,21 @@ class Solution:
     def balancedString(self, s: str) -> int:
         n = len(s)
         k = n // 4
-        freq = collections.Counter(s)
-        excess = {num: count - k for num, count in freq.items() if count > k}
+        window = collections.defaultdict(int)
+        target = collections.Counter(s)
+        excess = {char: count - k for char, count in target.items() if count > k}
         if not excess:
             return 0
-        window = collections.defaultdict(int)
-        have = 0
-        need = len(excess)
+        have, need = 0, len(excess)
         min_len = float('inf')
         l = 0
         for r in range(n):
             window[s[r]] += 1
-            if s[r] in excess and excess[s[r]] == window[s[r]]:
+            if s[r] in excess and window[s[r]] == excess[s[r]]:
                 have += 1
             while have == need:
                 min_len = min(min_len, r - l + 1)
-                if s[l] in excess and excess[s[l]] == window[s[l]]:
+                if s[l] in excess and window[s[l]] == excess[s[l]]:
                     have -= 1
                 window[s[l]] -= 1
                 l += 1
