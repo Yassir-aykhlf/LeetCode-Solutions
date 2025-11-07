735b65447a76f9bcf0fd18c62fd9bb44ddcb1ce9
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-07 18:15:59 +0100

    Time: 53 ms (17.60%) | Memory: 23.3 MB (21.08%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -1,9 +1,9 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
+        count = 0
         people.sort()
         l = 0
         r = len(people) - 1
-        count = 0
         while l <= r:
             if people[l] + people[r] <= limit:
                 l += 1
