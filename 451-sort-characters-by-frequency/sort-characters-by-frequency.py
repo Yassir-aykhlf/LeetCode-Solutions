0ad9d00cb863f9a5f33ff92acdf5760bb4e5fde9
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-08 16:37:22 +0100

    Time: 3 ms (97.63%) | Memory: 19.1 MB (47.18%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.py
@@ -1,8 +1,4 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        freq = {}
-        for c in s:
-            if c not in freq:
-                freq[c] = 0
-            freq[c] += 1
-        return ''.join(char * freq[char] for char in sorted(freq.keys(), key=lambda x:freq[x], reverse=True))
+        freq = collections.Counter(s)
+        return ''.join(char * count for char, count in sorted(freq.items(), key=itemgetter(1), reverse=True))
