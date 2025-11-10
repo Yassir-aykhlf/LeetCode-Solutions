c228ecb331dee638c42689b17ecf598b18106361
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-10 15:54:28 +0100

    Time: 4 ms (46.48%) | Memory: 19.3 MB (11.57%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,10 +10,11 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwo(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode();
-        ListNode* cur = dummy;
-        while (l1 && l2) {
+    ListNode* mergeTwo(ListNode* l1, ListNode *l2) {
+        ListNode dummy(0);
+        ListNode* cur = &dummy;
+
+        while (l1 != NULL && l2 != NULL) {
             if (l1->val < l2->val) {
                 cur->next = l1;
                 l1 = l1->next;
@@ -25,17 +26,17 @@ public:
             cur = cur->next;
         }
         cur->next = l1 ? l1 : l2;
-        return dummy->next;
+        return dummy.next;
     }
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.empty()) {
-            return nullptr;
+            return NULL;
         }
         while (lists.size() > 1) {
             vector<ListNode*> merged;
             for (int i = 0; i < lists.size(); i += 2) {
-                ListNode* l1 = lists[i];
-                ListNode* l2 = ((i + 1) < lists.size()) ? lists[i + 1] : nullptr;
+                ListNode *l1 = lists[i];
+                ListNode *l2 = (i + 1 < lists.size()) ? lists[i + 1] : NULL;
                 merged.push_back(mergeTwo(l1, l2));
             }
             lists = merged;
