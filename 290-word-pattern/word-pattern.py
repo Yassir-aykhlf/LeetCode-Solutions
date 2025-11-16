d3b385742249887cd773ccfbd7e44f47baeb130d
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-16 13:54:08 +0100

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.97%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -7,12 +7,10 @@ class Solution:
         char_word = {}
         word_char = {}
         for char, word in zip(chars, words):
-            if char in char_word:
-                if char_word[char] != word:
-                    return False
-            if word in word_char:
-                if word_char[word] != char:
-                    return False
+            if char in char_word and char_word[char] != word:
+                return False
+            if word in word_char and word_char[word] != char:
+                return False
             char_word[char] = word
             word_char[word] = char
         return True
