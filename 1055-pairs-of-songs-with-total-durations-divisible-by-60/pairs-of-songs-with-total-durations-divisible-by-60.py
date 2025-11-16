d7d1092b5f667729bcc5a97aca11040101ed18e0
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-16 13:43:20 +0100

    Time: 23 ms (52.00%) | Memory: 21.3 MB (27.32%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,17 +1,11 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        # pair is divisible by 60 if: sum(pair) % 60 == 0
-        # for each time we identify the complimenting pair, and see if we have seen one before
-        # if so, we can form pairs equal to how many time we have seen a number that it remainder is a valid pair
-        # num1 % 60 = 0; num2 % 60 = 0, sum(num1, num2) % = 60
-        remainders = defaultdict(int)
-        # {rem: count_of_numbers_that_give_this_remainder}
+        remainders = {}
         count = 0
         for t in time:
             rem = t % 60
-            target_pair = (60 - t) % 60
-            # t + t_p = 60; we are looking for a pair-remainder
-            # the % 60 is to insure the result remains in range [0, 59]
-            count += remainders[target_pair]
-            remainders[rem] += 1
+            target_rem = (60 - rem) % 60
+            if target_rem in remainders:
+                count += remainders[target_rem]
+            remainders[rem] = remainders.get(rem, 0) + 1
         return count
