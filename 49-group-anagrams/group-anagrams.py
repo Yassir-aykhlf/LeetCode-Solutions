9031b145f792f3aac516fc9ba1ea915cfc4e067a
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-19 11:41:25 +0100

    Time: 35 ms (9.07%) | Memory: 20.7 MB (74.98%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,7 +1,18 @@
 class Solution:
+    def genKey(self, str_: str) -> str:
+        alpha = [0] * 26
+        key = ""
+        for c in str_:
+            alpha[ord(c) - ord('a')] += 1
+        for i, c in enumerate(alpha):
+            if c > 0:
+                key += f"{c}{chr(i + ord('a'))}"
+        return key
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        group = defaultdict(list)
+        groups = {}
         for str_ in strs:
-            key = ''.join(sorted(str_))
-            group[key].append(str_)
-        return [lst for lst in group.values()]
+            key = self.genKey(str_)
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(str_)
+        return [group for group in groups.values()]
