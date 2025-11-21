cf17ab07e94156b1f583dcc14f868ff2eda8a1dd
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-21 14:09:28 +0100

    Time: 0 ms (100.00%) | Memory: 17.9 MB (70.31%) - LeetSync

--- 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -1,12 +1,12 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         bucket = defaultdict(list)
-        group = []
-        for id_, size in enumerate(groupSizes):
-            if len(bucket[size]) >= size:
-                group.append(bucket[size])
-                bucket[size] = []
-            bucket[size].append(id_)
+        res = []
+        for id_, size_ in enumerate(groupSizes):
+            if len(bucket[size_]) >= size_:
+                res.append(bucket[size_])
+                bucket[size_] = []
+            bucket[size_].append(id_)
         for g in bucket.values():
-            group.append(g)
-        return group
+            res.append(g)
+        return res
