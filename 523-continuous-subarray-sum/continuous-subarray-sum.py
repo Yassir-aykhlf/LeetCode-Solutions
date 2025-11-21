2985298c2ced8262fb1df57afe7b4e7c853e0e18
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-21 13:59:56 +0100

    Time: 55 ms (38.13%) | Memory: 38.2 MB (14.28%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,15 +1,13 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        prefix = collections.defaultdict(int)
-        prefix[0] = -1
+        rems = {0: -1}
         cur_sum = 0
-        count = 0
         for i, num in enumerate(nums):
             cur_sum += num
-            cur_rem = cur_sum % k
-            if cur_rem in prefix:
-                if i - prefix[cur_rem] >= 2:
+            rem = cur_sum % k
+            if rem in rems:
+                if i - rems[rem] >= 2:
                     return True
             else:
-                prefix[cur_rem] = i
+                rems[rem] = i
         return False
