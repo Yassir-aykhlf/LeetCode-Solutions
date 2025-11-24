3db1d515551032b202c7a9a89ff5f722137d93c9
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-24 11:41:44 +0100

    Time: 51 ms (39.20%) | Memory: 28.8 MB (86.83%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,17 +1,14 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
-        # clean the array
         for i in range(n):
             if nums[i] < 1 or nums[i] > n:
                 nums[i] = n + 1
-        # mark num - 1 in nums[num - 1]
         for i in range(n):
             index = abs(nums[i]) - 1
             if index >= 0 and index < n:
                 if nums[index] > 0:
                     nums[index] *= -1
-        # search for the first umarked i
         for i in range(n):
             if nums[i] > 0:
                 return i + 1
