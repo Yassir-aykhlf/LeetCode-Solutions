ae76b67f6e5326a8ee6f6c7268771673b225db87
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-26 12:10:14 +0100

    Time: 0 ms (100.00%) | Memory: 18 MB (12.34%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -10,7 +10,7 @@ class Solution:
         for _ in range(n):
             fast = fast.next
         while fast and fast.next:
-            fast = fast.next
             slow = slow.next
+            fast = fast.next
         slow.next = slow.next.next
         return dummy.next
