dfe97690da5995b3aa29601bc147a2e92f4502e1
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-11-26 12:41:58 +0100

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.48%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.py
+++ 926-find-and-replace-pattern/find-and-replace-pattern.py
@@ -1,16 +1,15 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
-        def isIsomorphic(word):
-            if len(word) != len(pattern):
-                return False
-            w_p = {}
-            p_w = {}
-            for w, p in zip(word, pattern):
-                if w in w_p and w_p[w] != p:
+        def isIso(w1, w2):
+            if len(w1) != len(w2): return False
+            w1_w2 = {}
+            w2_w1 = {}
+            for c1, c2 in zip(w1, w2):
+                if c1 in w1_w2 and w1_w2[c1] != c2:
                     return False
-                if p in p_w and p_w[p] != w:
+                if c2 in w2_w1 and w2_w1[c2] != c1:
                     return False
-                w_p[w] = p
-                p_w[p] = w
+                w1_w2[c1] = c2
+                w2_w1[c2] = c1
             return True
-        return [word for word in words if isIsomorphic(word)]
+        return [word for word in words if isIso(word, pattern)]
