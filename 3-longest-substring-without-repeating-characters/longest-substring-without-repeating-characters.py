47767223c9b6bdf48b1b3496ceefd7266c960562
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-02 09:36:41 +0100

    Time: 30 ms (19.41%) | Memory: 18 MB (33.76%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,9 +4,11 @@ class Solution:
         max_len = 0
         l = 0
         for r in range(len(s)):
-            window[s[r]] += 1
-            while window[s[r]] > 1:
+            while s[r] in window:
                 window[s[l]] -= 1
+                if window[s[l]] == 0:
+                    del window[s[l]]
                 l += 1
+            window[s[r]] += 1
             max_len = max(max_len, r - l + 1)
         return max_len
