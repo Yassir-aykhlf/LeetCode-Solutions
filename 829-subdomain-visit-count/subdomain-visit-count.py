f3043bf3ba8c45d5714b8708c1dd007737819556
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-02 15:20:05 +0100

    Time: 7 ms (44.62%) | Memory: 17.7 MB (96.41%) - LeetSync

--- 829-subdomain-visit-count/subdomain-visit-count.py
+++ 829-subdomain-visit-count/subdomain-visit-count.py
@@ -1,11 +1,11 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        domains = defaultdict(int)
+        doms = defaultdict(int)
         for cpdomain in cpdomains:
             count_str, domain = cpdomain.split()
             count = int(count_str)
-            domains[domain] += count
+            doms[domain] += count
             for i, c in enumerate(domain):
-                if c == '.':
-                    domains[domain[i+1:]] += count
-        return [f"{count} {domain}" for domain, count in domains.items()]
+                if c == ".":
+                    doms[domain[i+1:]] += count
+        return [f"{count} {dom}" for dom, count in doms.items()]
