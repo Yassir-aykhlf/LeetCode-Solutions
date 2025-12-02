f15dd93d25fa5f9c0c002e4160c1cef409751bfd
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-02 15:39:08 +0100

    Time: 74 ms (57.25%) | Memory: 18.3 MB (53.93%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,20 +1,20 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        target = collections.Counter(t)
+        window = collections.defaultdict(int)
+        have, need = 0, len(target)
         min_len = float("inf")
-        ref = Counter(t)
-        window = defaultdict(int)
-        have, need = 0, len(ref)
-        l = 0
         res = (0, 0)
+        l = 0
         for r in range(len(s)):
             window[s[r]] += 1
-            if s[r] in ref and ref[s[r]] == window[s[r]]:
+            if s[r] in target and target[s[r]] == window[s[r]]:
                 have += 1
             while have == need:
                 if r - l + 1 < min_len:
                     min_len = r - l + 1
                     res = (l, r + 1)
-                if s[l] in ref and ref[s[l]] == window[s[l]]:
+                if s[l] in target and target[s[l]] == window[s[l]]:
                     have -= 1
                 window[s[l]] -= 1
                 l += 1
