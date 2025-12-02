9c519d3f472dd943abd48c9d78afee9266b3c4da
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-02 12:33:29 +0100

    Time: 579 ms (56.79%) | Memory: 20.6 MB (87.07%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,25 +3,23 @@ class Solution:
         nums.sort()
         n = len(nums)
         res = []
-        for i in range(n):
-            if i > 0  and nums[i] == nums[i - 1]:
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            if nums[i] > 0:
-                return res
-            a = i + 1
-            b = n - 1
-            while a < b:
-                result = nums[i] + nums[a] + nums[b]
-                if result == 0:
-                    res.append([nums[i], nums[a], nums[b]])
-                    while a < b and nums[a] == nums[a + 1]:
-                        a += 1
-                    while a < b and nums[b] == nums[b - 1]:
-                        b -= 1
-                    a += 1
-                    b -= 1
-                elif result < 0:
-                    a += 1
+            l = i + 1
+            r = n - 1
+            while l < r:
+                comb = nums[i] + nums[l] + nums[r]
+                if comb == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif comb < 0:
+                    l += 1
                 else:
-                    b -= 1
+                    r -= 1
         return res
