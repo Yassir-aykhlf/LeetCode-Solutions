5b3cd148d1164746ace1d1efc094d8c9ab9e383e
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-02 10:29:27 +0100

    Time: 32 ms (34.06%) | Memory: 21.2 MB (72.16%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,11 +1,12 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        rems = {0: 1}
-        cur_sum, count = 0, 0
-        for num in nums:
-            cur_sum += num
-            cur_rem = cur_sum % k
-            if cur_rem in rems:
-                count += rems[cur_rem]
-            rems[cur_rem] = rems.get(cur_rem, 0) + 1
+        rems = {0:1}
+        count = 0
+        cur_sum = 0
+        for r in range(len(nums)):
+            cur_sum += nums[r]
+            rem = cur_sum % k
+            if rem in rems:
+                count += rems[rem]
+            rems[rem] = rems.get(rem, 0) + 1
         return count
