c8eb7bc0b9cde5a1b01b27657d1e16046ef42700
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-08 15:11:35 +0100

    Time: 0 ms (100.00%) | Memory: 11.1 MB (16.67%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.c
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.c
@@ -1,14 +1,10 @@
 int numPairsDivisibleBy60(int* time, int timeSize) {
-    int rems[60];
-    bzero(rems, 60);
-    int cur_sum = 0;
+    int rems[60] = {0};
     int count = 0;
     for (int i = 0; i < timeSize; ++i) {
         int cur_rem = time[i] % 60;
         int target_rem = (60 - cur_rem) % 60;
-        if (rems[target_rem] > 0) {
-            count += rems[target_rem];
-        }
+        count += rems[target_rem];
         rems[cur_rem] += 1;
     }
     return count;
