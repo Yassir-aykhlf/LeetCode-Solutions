f4c945e83fbc8c99869daa81f6364d79e5ffb9c8
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-08 19:44:02 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.06%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,5 +1,4 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        freq = collections.Counter(words)
-        tuples = list(freq.items())
-        return [w for w, f in sorted(tuples, key = lambda t: (-t[1], t[0]))[:k]]
+        freq = Counter(words)
+        return sorted(freq.keys(), key=lambda k: (-freq[k], k))[:k]
