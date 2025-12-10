c1d3594628ad9cb4a7b4adc3c247543d248d0669
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-10 17:05:25 +0100

    Time: 3 ms (95.95%) | Memory: 18 MB (35.50%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -3,8 +3,8 @@ class Solution:
         s_t = {}
         t_s = {}
         for s_c, t_c in zip(s, t):
-           if (s_c in s_t and s_t[s_c] != t_c) or (t_c in t_s and t_s[t_c] != s_c):
-            return False 
-           s_t[s_c] = t_c
-           t_s[t_c] = s_c
+            if (s_c in s_t and s_t[s_c] != t_c) or (t_c in t_s and t_s[t_c] != s_c):
+                return False
+            s_t[s_c] = t_c
+            t_s[t_c] = s_c
         return True
