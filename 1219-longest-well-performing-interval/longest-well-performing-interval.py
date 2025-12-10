6029a6d13e056cd8b02b7bd80638c15b962ff906
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-10 15:52:37 +0100

    Time: 19 ms (86.98%) | Memory: 18.1 MB (87.22%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.py
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.py
@@ -6,7 +6,7 @@ class Solution:
         for i, hour in enumerate(hours):
             curr += 1 if hour > 8 else -1
             if curr > 0:
-                max_len = max(max_len, i + 1)
+                max_len = i + 1
             elif (curr - 1) in prefix:
                 max_len = max(max_len, i - prefix[curr - 1])
             if curr not in prefix:
