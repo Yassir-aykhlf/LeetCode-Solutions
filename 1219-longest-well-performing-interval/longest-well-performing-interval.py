66e914973bd8027bd530a888776abefa01007941
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-11 09:53:35 +0100

    Time: 23 ms (62.72%) | Memory: 18.2 MB (71.11%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.py
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.py
@@ -1,14 +1,16 @@
 class Solution:
     def longestWPI(self, hours: List[int]) -> int:
+        # [9,9,6,0,6,6,9] => [1,1,-1,-1,-1,-1,1] because we don't care about the values, we care about the condition: h > 8
+        # why -1 and not 0? ...
         curr = 0
-        max_len = 0
+        longest = 0
         prefix = {0: -1}
-        for i, hour in enumerate(hours):
-            curr += 1 if hour > 8 else -1
+        for i, h in enumerate(hours):
+            curr += 1 if h > 8 else -1
             if curr > 0:
-                max_len = i + 1
+                longest = i + 1
             elif (curr - 1) in prefix:
-                max_len = max(max_len, i - prefix[curr - 1])
+                longest = max(longest, i - prefix[curr - 1])
             if curr not in prefix:
                 prefix[curr] = i
-        return max_len
+        return longest
