da6b503c44aba39704600ab290c5f41cd281bd2a
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-11 14:38:26 +0100

    Time: 58 ms (88.23%) | Memory: 173.2 MB (70.97%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,39 @@
+class LRUCache {
+private:
+    int capacity;
+    std::list<std::pair<int, int>> cacheList;
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> cacheMap;
+
+public:
+    LRUCache(int capacity) : capacity(capacity) {}
+
+    int get(int key) {
+        if (cacheMap.find(key) == cacheMap.end()) {
+            return -1;
+        }
+        cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+        return cacheMap[key]->second;
+    }
+
+    void put(int key, int value) {
+        if (cacheMap.find(key) != cacheMap.end()) {
+            cacheMap[key]->second = value;
+            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+            return;
+        }
+        if (cacheList.size() == capacity) {
+            int keyToRemove = cacheList.back().first;
+            cacheList.pop_back();
+            cacheMap.erase(keyToRemove);
+        }
+        cacheList.push_front({key, value});
+        cacheMap[key] = cacheList.begin();
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
