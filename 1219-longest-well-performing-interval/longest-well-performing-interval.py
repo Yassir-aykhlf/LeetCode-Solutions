5388f459449078e9432e52182b1dd51e65e7bd98
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-11 10:00:52 +0100

    Time: 19 ms (87.16%) | Memory: 18.3 MB (71.11%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.py
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.py
@@ -1,7 +1,8 @@
 class Solution:
     def longestWPI(self, hours: List[int]) -> int:
         # [9,9,6,0,6,6,9] => [1,1,-1,-1,-1,-1,1] because we don't care about the values, we care about the condition: h > 8
-        # why -1 and not 0? ...
+        # why -1 and not 0? because we want to find an interval with strict positive sum, each low performing day negatively affect the score
+        # we also care about the "velocity" rather than the score, so if we have already seen (curr - 1) it means we are going up, therefore having more productive days, and since we are looking for the longest productive window curr - 1 yeilds the farthest one
         curr = 0
         longest = 0
         prefix = {0: -1}
