5b514537fe3664dfee12a2ced7428ab4111942ae
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-11 21:41:27 +0100

    Time: 227 ms (79.57%) | Memory: 19.6 MB (77.92%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.py
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -1,17 +1,16 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        char_words = defaultdict(list)
-        for word in words:
-            char_words[word[0]].append([word, 0])
         count = 0
+        alpha = defaultdict(list)
+        for word in words:
+            alpha[word[0]].append([word, 0])
         for c in s:
-            group = char_words[c]
-            char_words[c] = []
+            group = alpha[c]
+            alpha[c] = []
             for pair in group:
-                word, index = pair
-                index += 1
-                if index == len(word):
+                pair[1] += 1
+                if len(pair[0]) == pair[1]:
                     count += 1
                 else:
-                    char_words[word[index]].append([word, index])
+                    alpha[pair[0][pair[1]]].append(pair)
         return count
