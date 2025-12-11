52df41788aea8d9b8356a280134833ffb3bfe7fe
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-11 15:25:10 +0100

    Time: 72 ms (68.01%) | Memory: 173.2 MB (70.97%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -3,28 +3,27 @@ private:
     int capacity;
     std::list<std::pair<int, int>> cacheList;
     std::unordered_map<int, std::list<std::pair<int, int>>::iterator> cacheMap;
-
 public:
     LRUCache(int capacity) : capacity(capacity) {}
-
+    
     int get(int key) {
         if (cacheMap.find(key) == cacheMap.end()) {
             return -1;
         }
         cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
-        return cacheMap[key]->second;
+        return cacheList.begin()->second;
     }
-
+    
     void put(int key, int value) {
         if (cacheMap.find(key) != cacheMap.end()) {
-            cacheMap[key]->second = value;
             cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
+            cacheList.begin()->second = value;
             return;
         }
         if (cacheList.size() == capacity) {
-            int keyToRemove = cacheList.back().first;
+            auto del = cacheList.back().first;
             cacheList.pop_back();
-            cacheMap.erase(keyToRemove);
+            cacheMap.erase(del);
         }
         cacheList.push_front({key, value});
         cacheMap[key] = cacheList.begin();
