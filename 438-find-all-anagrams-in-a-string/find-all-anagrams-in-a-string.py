60ed07164977d3e5d12807acad20e2f7f03f1147
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-11 21:31:50 +0100

    Time: 215 ms (33.15%) | Memory: 18.5 MB (35.55%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,16 +1,17 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        n = len(s)
-        k = len(p)
-        ref = Counter(p)
-        window = Counter(s[:k])
-        res = [0] if window == ref else []
+        l_p = len(p)
+        l_s = len(s)
+        target = collections.Counter(p)
+        window = collections.Counter(s[:l_p])
+        res = [0] if target == window else []
         l = 0
-        for r in range(k, n):
+        for r in range(l_p, l_s):
             window[s[r]] += 1
-            window[s[r - k]] -= 1
-            if window[s[r - k]] == 0:
-                del window[s[r - k]]
-            if window == ref:
-                res.append(r - k + 1)
+            window[s[l]] -= 1
+            if window[s[l]] == 0:
+                del window[s[l]]
+            if window == target:
+                res.append(l + 1)
+            l += 1
         return res
