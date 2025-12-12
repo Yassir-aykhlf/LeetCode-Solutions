98a5c9216a361a41e5de475b04729000cf9e4da3
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-12 09:44:48 +0100

    Time: 20 ms (70.86%) | Memory: 60.9 MB (66.25%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.cpp
@@ -0,0 +1,50 @@
+class MyHashMap {
+    const int size = 997;
+    std::vector<std::list<std::pair<int, int>>> bucket;
+    int hash(int key) {
+        return key % size;
+    }
+public:
+    MyHashMap() {
+        bucket.resize(size);
+    }
+    
+    void put(int key, int value) {
+        auto &chain = bucket[hash(key)];
+        for (auto &el : chain) {
+            if (el.first == key) {
+                el.second = value;
+                return;
+            }
+        }
+        chain.push_back({key, value});
+    }
+    
+    int get(int key) {
+        auto &chain = bucket[hash(key)];
+        for (auto &el : chain) {
+            if (el.first == key) {
+                return el.second;
+            }
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        auto &chain = bucket[hash(key)];
+        for (auto itr = chain.begin(); itr != chain.end(); ++itr) {
+            if (itr->first == key) {
+                chain.erase(itr);
+                return;
+            }
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
