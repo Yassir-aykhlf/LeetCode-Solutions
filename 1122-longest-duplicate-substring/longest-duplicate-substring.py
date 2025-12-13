ceb7e72933c975d8b4a649f13c93951073bb0f13
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-13 18:50:51 +0100

    Time: 1060 ms (67.56%) | Memory: 21.8 MB (92.42%) - LeetSync

--- 1122-longest-duplicate-substring/longest-duplicate-substring.py
+++ 1122-longest-duplicate-substring/longest-duplicate-substring.py
@@ -1,36 +1,32 @@
 class Solution:
     def longestDupSubstring(self, s: str) -> str:
         def check(n):
-            seen = {}
-            MOD = 10**9 + 7
-            BASE = 26
-            highest_pow = pow(BASE, n - 1, MOD)
+            seen = set()
+            mod = 2**63 - 1
+            base = 26
+            highest_pow = pow(base, n - 1, mod)
             r_hash = 0
             for i in range(n):
-                r_hash = (r_hash * BASE + (ord(s[i]) - ord('a'))) % MOD
-            seen[r_hash] = 0
+                r_hash = (r_hash * base + (ord(s[i]) - ord('a'))) % mod
+            seen.add(r_hash)
             for i in range(1, len(s) - n + 1):
-                old = (ord(s[i - 1]) - ord('a'))
-                new = (ord(s[i + n - 1]) - ord('a'))
-                r_hash = ((r_hash - old * highest_pow) * BASE + new) % MOD
+                old = ord(s[i - 1]) - ord('a')
+                new = ord(s[i + n - 1]) - ord('a')
+                r_hash = (((r_hash - old * highest_pow) * base) + new) % mod
                 if r_hash in seen:
-                    prev_i = seen[r_hash]
-                    if s[prev_i : prev_i + n] == s[i : i + n]:
-                        return i
-                else:
-                    seen[r_hash] = i
+                    return i
+                seen.add(r_hash)
             return -1
-        
-        l, r = 1, len(s) - 1
-        best_start = -1
-        best_len = 0
+        l, r = 0, len(s) - 1
+        best_len = -1
+        best_index = 0
         while l <= r:
             mid = (l + r) // 2
             index = check(mid)
             if index != -1:
-                best_start = index
                 best_len = mid
+                best_index = index
                 l = mid + 1
             else:
                 r = mid - 1
-        return s[best_start: best_start + best_len] if best_start != -1 else ""
+        return s[best_index : best_index + best_len] if best_len != -1 else ""
