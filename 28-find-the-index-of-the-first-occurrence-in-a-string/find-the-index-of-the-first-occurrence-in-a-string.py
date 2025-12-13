40549bbd8cabfff04188662ee11d159c8ccb95d6
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-13 10:44:56 +0100

    Time: 3 ms (10.82%) | Memory: 17.8 MB (33.68%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -2,8 +2,31 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         # return haystack.find(needle)
 
+        # n, m = len(haystack), len(needle)
+        # for i in range(n - m + 1):
+        #     if haystack[i: i + m] == needle:
+        #         return i
+        # return -1
+
         n, m = len(haystack), len(needle)
-        for i in range(n - m + 1):
-            if haystack[i: i + m] == needle:
-                return i
+        if not needle: return 0
+        if m > n: return -1
+        MOD = 10**9 + 7
+        BASE = 26
+        highest_pow = pow(BASE, m - 1, MOD)
+        n_hash = 0
+        h_hash = 0
+        for i in range(m):
+            n_hash = (n_hash * BASE + (ord(needle[i]) - ord('a'))) % MOD
+            h_hash = (h_hash * BASE + (ord(haystack[i]) - ord('a'))) % MOD
+        if n_hash == h_hash:
+            if haystack[:m] == needle:
+                return 0
+        for i in range(1, n - m + 1):
+            old = (ord(haystack[i - 1]) - ord('a')) * highest_pow
+            new = ord(haystack[i + m - 1]) - ord('a')
+            h_hash = ((h_hash - old) * BASE + new) % MOD
+            if h_hash == n_hash:
+                if haystack[i: i+m] == needle:
+                    return i
         return -1
