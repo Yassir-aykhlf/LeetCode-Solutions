20480ebf8d448c6643b2c26e01009df62ca3f7b6
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-13 11:31:58 +0100

    Time: 851 ms (83.15%) | Memory: 22.2 MB (81.88%) - LeetSync

--- 1122-longest-duplicate-substring/longest-duplicate-substring.py
+++ 1122-longest-duplicate-substring/longest-duplicate-substring.py
@@ -1,31 +1,36 @@
 class Solution:
     def longestDupSubstring(self, s: str) -> str:
-        def check(length):
-            seen = set()
-            current_hash = 0
+        def check(n):
+            seen = {}
+            MOD = 10**9 + 7
             BASE = 26
-            MOD = 2**63 - 1
-            highest_pow = pow(BASE, length - 1, MOD)
-            for i in range(length):
-                current_hash = (current_hash * BASE + (ord(s[i]) - ord('a'))) % MOD
-            seen.add(current_hash)
-            for i in range(1, len(s) - length + 1):
-                prev = ord(s[i - 1]) - ord('a')
-                new  = ord(s[i + length - 1]) - ord('a')
-                current_hash = (current_hash - prev * highest_pow) % MOD
-                current_hash = (current_hash * BASE + new) % MOD
-                if current_hash in seen:
-                    return i
-                seen.add(current_hash)
+            highest_pow = pow(BASE, n - 1, MOD)
+            r_hash = 0
+            for i in range(n):
+                r_hash = (r_hash * BASE + (ord(s[i]) - ord('a'))) % MOD
+            seen[r_hash] = 0
+            for i in range(1, len(s) - n + 1):
+                old = (ord(s[i - 1]) - ord('a'))
+                new = (ord(s[i + n - 1]) - ord('a'))
+                r_hash = ((r_hash - old * highest_pow) * BASE + new) % MOD
+                if r_hash in seen:
+                    prev_i = seen[r_hash]
+                    if s[prev_i : prev_i + n] == s[i : i + n]:
+                        return i
+                else:
+                    seen[r_hash] = i
             return -1
-        low, high = 1, len(s) - 1
+        
+        l, r = 1, len(s) - 1
         best_start = -1
-        while low <= high:
-            mid = (low + high) // 2
-            found = check(mid)
-            if found != -1:
-                best_start = found
-                low = mid + 1
+        best_len = 0
+        while l <= r:
+            mid = (l + r) // 2
+            index = check(mid)
+            if index != -1:
+                best_start = index
+                best_len = mid
+                l = mid + 1
             else:
-                high = mid - 1
-        return s[best_start : best_start + (low - 1)] if best_start != -1 else ""
+                r = mid - 1
+        return s[best_start: best_start + best_len] if best_start != -1 else ""
