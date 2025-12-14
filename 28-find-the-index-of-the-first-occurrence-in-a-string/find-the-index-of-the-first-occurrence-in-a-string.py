337459adf29d045229f049f091375e96c9151a44
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-14 17:08:46 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.58%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,23 +1,29 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if not needle:
-            return 0
-        m, n = len(needle), len(haystack)
-        if m > n:
-            return -1
-        mod = 10**9 + 7
-        base = 26
-        highest_pow = pow(base, m - 1, mod)
-        n_hash, h_hash = 0, 0
-        for i in range(m):
-            n_hash = (n_hash * base + ord(needle[i])) % mod
-            h_hash = (h_hash * base + ord(haystack[i])) % mod
-        if n_hash == h_hash and haystack[:m] == needle:
-            return 0
-        for i in range(1, n - m + 1):
-            old = ord(haystack[i - 1])
-            new = ord(haystack[i + m - 1])
-            h_hash = ((h_hash - old * highest_pow) * base + new) % mod
-            if h_hash == n_hash and haystack[i : i+m] == needle:
+        # if not needle:
+        #     return 0
+        # m, n = len(needle), len(haystack)
+        # if m > n:
+        #     return -1
+        # mod = 10**9 + 7
+        # base = 26
+        # highest_pow = pow(base, m - 1, mod)
+        # n_hash, h_hash = 0, 0
+        # for i in range(m):
+        #     n_hash = (n_hash * base + ord(needle[i])) % mod
+        #     h_hash = (h_hash * base + ord(haystack[i])) % mod
+        # if n_hash == h_hash and haystack[:m] == needle:
+        #     return 0
+        # for i in range(1, n - m + 1):
+        #     old = ord(haystack[i - 1])
+        #     new = ord(haystack[i + m - 1])
+        #     h_hash = ((h_hash - old * highest_pow) * base + new) % mod
+        #     if h_hash == n_hash and haystack[i : i+m] == needle:
+        #         return i
+        # return -1
+
+        n, m = len(haystack), len(needle)
+        for i in range(n - m + 1):
+            if haystack[i: i+m] == needle:
                 return i
         return -1
