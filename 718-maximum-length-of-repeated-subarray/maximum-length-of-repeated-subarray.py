73d54b644e16b9a761aa939ad6b8a4fdc4ccb33b
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-14 17:27:40 +0100

    Time: 24 ms (99.81%) | Memory: 17.9 MB (87.98%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,43 +1,38 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        def check(length):
-            if length == 0:
-                return True
-
+        def check(n):
             seen = set()
-            BASE = 101
-            MOD = 10**9 + 7
-            highest_pow = pow(BASE, length - 1, MOD)
-
-            h1 = 0
-            for i in range(length):
-                h1 = (h1 * BASE + nums1[i]) % MOD
-            seen.add(h1)
-            for i in range(1, len(nums1) - length + 1):
-                h1 = (h1 - nums1[i - 1] * highest_pow) % MOD
-                h1 = (h1 * BASE + nums1[i + length - 1]) % MOD
-                seen.add(h1)
-
-            h2 = 0
-            for i in range(length):
-                h2 = (h2 * BASE + nums2[i]) % MOD
-            if h2 in seen:
+            hash1, hash2 = 0, 0
+            base = 101
+            mod = 10**9 + 1
+            highest_pow = pow(base, n - 1, mod)
+            for i in range(n):
+                hash1 = (hash1 * base + nums1[i]) % mod
+            seen.add(hash1)
+            for i in range(1, len(nums1) - n + 1):
+                old = nums1[i - 1]
+                new = nums1[i + n - 1]
+                hash1 = ((hash1 - old * highest_pow) * base + new) % mod
+                seen.add(hash1)
+            for i in range(n):
+                hash2 = (hash2 * base + nums2[i]) % mod
+            if hash2 in seen:
                 return True
-            for i in range(1, len(nums2) - length + 1):
-                h2 = (h2 - nums2[i - 1] * highest_pow) % MOD
-                h2 = (h2 * BASE + nums2[i + length - 1]) % MOD
-                if h2 in seen:
+            for i in range(1, len(nums2) - n + 1):
+                old = nums2[i - 1]
+                new = nums2[i + n - 1]
+                hash2 = ((hash2 - old * highest_pow) * base + new) % mod
+                if hash2 in seen:
                     return True
-                    
             return False
-
+        # 
         low, high = 0, min(len(nums1), len(nums2))
         ans = 0
         while low <= high:
             mid = (low + high) // 2
             if check(mid):
-                ans  = mid
-                low  = mid + 1
+                ans = mid
+                low = mid + 1
             else:
                 high = mid - 1
         return ans
