42b7f9a200058974a3ceec7fa45fd2065d8c22f8
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-14 18:41:46 +0100

    Time: 23 ms (66.51%) | Memory: 29.2 MB (47.18%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,22 +1,12 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        n = len(s)
-        if n < 10: return []
-        m = 10
-        base = 4
-        mod = 10**9 + 7
-        highest_pow = pow(base, m - 1, mod)
-        to_int = {"A":0, "C":1, "G":2, "T":3}
-        nums = [to_int[c] for c in s]
-        dna_hash = 0
         seen = set()
-        res = set()
-        for i in range(m):
-            dna_hash = (dna_hash * base + nums[i]) % mod
-        seen.add(dna_hash)
-        for i in range(1, n - m + 1):
-            dna_hash = ((dna_hash - nums[i - 1] * highest_pow) * base + nums[i + m - 1]) % mod
-            if dna_hash in seen:
-                res.add(s[i: i + m])
-            seen.add(dna_hash)
-        return list(res)
+        repeated = set()
+        n = len(s)
+        for i in range(n - 9):
+            sub = s[i: i+10]
+            if sub in seen:
+                repeated.add(sub)
+            else:
+                seen.add(sub)
+        return list(repeated)
