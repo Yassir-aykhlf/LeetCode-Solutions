92d5542ed8abd3d8403f9da6f5fbfc283850caa9
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-17 12:29:17 +0100

    Time: 24 ms (99.81%) | Memory: 18.1 MB (81.55%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,31 +1,31 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         def check(n):
-            seen = set()
-            hash1, hash2 = 0, 0
+            mod = 10**9 + 7
             base = 101
-            mod = 10**9 + 1
             highest_pow = pow(base, n - 1, mod)
+            hash1 = 0
+            hash2 = 0
+            seen = set()
             for i in range(n):
                 hash1 = (hash1 * base + nums1[i]) % mod
             seen.add(hash1)
             for i in range(1, len(nums1) - n + 1):
-                old = nums1[i - 1]
-                new = nums1[i + n - 1]
-                hash1 = ((hash1 - old * highest_pow) * base + new) % mod
+                old = nums1[i - 1] * highest_pow % mod
+                new = nums1[i + n - 1] % mod
+                hash1 = ((hash1 - old) * base + new) % mod
                 seen.add(hash1)
             for i in range(n):
                 hash2 = (hash2 * base + nums2[i]) % mod
             if hash2 in seen:
                 return True
             for i in range(1, len(nums2) - n + 1):
-                old = nums2[i - 1]
-                new = nums2[i + n - 1]
-                hash2 = ((hash2 - old * highest_pow) * base + new) % mod
+                old = nums2[i - 1] * highest_pow % mod
+                new = nums2[i + n - 1] % mod
+                hash2 = ((hash2 - old) * base + new) % mod
                 if hash2 in seen:
                     return True
             return False
-        # 
         low, high = 0, min(len(nums1), len(nums2))
         ans = 0
         while low <= high:
