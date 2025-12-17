99dc473cd867cded0ed52ab829f4505179f459f7
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-17 11:51:26 +0100

    Time: 737 ms (95.84%) | Memory: 22.2 MB (82.21%) - LeetSync

--- 1122-longest-duplicate-substring/longest-duplicate-substring.py
+++ 1122-longest-duplicate-substring/longest-duplicate-substring.py
@@ -1,33 +1,32 @@
 class Solution:
+    def check(self, s: str, n: int) -> int:
+        mod = 10**9 + 7
+        base = 31
+        highest_pow = pow(base, n - 1, mod)
+        r_hash = 0
+        seen = {}
+        for i in range(n):
+            r_hash = (r_hash * base + ord(s[i])) % mod
+        seen[r_hash] = 0
+        for i in range(1, len(s) - n + 1):
+            old = (ord(s[i - 1]) * highest_pow) % mod
+            new = ord(s[i + n - 1])
+            r_hash = ((r_hash - old) * base + new) % mod
+            if r_hash in seen and s[seen[r_hash] : seen[r_hash] + n] == s[i : i + n]:
+                return i
+            seen[r_hash] = i
+        return -1
     def longestDupSubstring(self, s: str) -> str:
-        def check(n):
-            base = 26
-            mod = 2**63 - 7
-            highest_pow = pow(base, n - 1, mod)
-            r_hash = 0
-            seen = set()
-            for i in range(n):
-                r_hash = (r_hash * base + (ord(s[i]) - ord('a'))) % mod
-            seen.add(r_hash)
-            for i in range(1, len(s) - n + 1):
-                old = ord(s[i - 1]) - ord('a')
-                new = ord(s[i + n - 1]) - ord('a')
-                r_hash = (((r_hash - (old * highest_pow)) * base) + new) % mod
-                if r_hash in seen:
-                    return i
-                seen.add(r_hash)
-            return -1
-        
-        l, r = 0, len(s) - 1
+        best_index = -1
         best_len = 0
-        best_i = -1
-        while l <= r:
-            mid = (l + r) // 2
-            index = check(mid)
+        low, high = 0, len(s) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            index = self.check(s, mid)
             if index != -1:
-                best_i = index
                 best_len = mid
-                l = mid + 1
+                best_index = index
+                low = mid + 1
             else:
-                r = mid - 1
-        return s[best_i : best_i + best_len] if best_i != -1 else ""
+                high = mid - 1
+        return s[best_index : best_index + best_len] if best_index != -1 else ""
