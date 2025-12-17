040a9439c029832480929ba307ceb760cd454b77
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-17 10:43:28 +0100

    Time: 437 ms (56.35%) | Memory: 119.2 MB (51.73%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        try {
-            std::set<std::string> seen;
-            for (int i = 0; i < s.size() - k + 1; ++i)
-                seen.insert(s.substr(i, k));
-            return seen.size() == std::pow(2, k);
-        } catch (std::exception &e) {
-            std::cout << "You done fucked up! " << e.what();
-        }
-        return false;
+        if (s.size() < k)
+            return false;
+        std::unordered_set<std::string> seen;
+        for (int i = 0; i < s.size() - k + 1; ++i)
+            seen.insert(s.substr(i, k));
+        return seen.size() == (1 << k);
     }
 };
