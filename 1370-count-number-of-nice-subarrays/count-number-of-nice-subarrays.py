c0b61aa9023cb341f69c7c55816231c9228c5f22
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-21 18:24:07 +0100

    Time: 107 ms (52.72%) | Memory: 23.8 MB (43.84%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,17 +1,12 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        # the property we care about is parity, so we can ignore the values
-        # and replace the value of odd numbers with 1, and even numbers with 0
-        # [1,1,2,1,1] => [1,1,0,1,1]
-        # k odd numbers subarray is a subarray with the sum equal to k
-        # rendering this problem a simple "subarray sum equals k" problem
-        prefix = {0: 1}
+        prefix_sum = {0: 1}
         curr = 0
         count = 0
-        for num in nums:
-            curr += 1 if num % 2 != 0 else 0
+        for i, num in enumerate(nums):
+            curr += 1 if num % 2 == 1 else 0
             target = curr - k
-            if target in prefix:
-                count += prefix[target]
-            prefix[curr] = prefix.get(curr, 0) + 1
+            if target in prefix_sum:
+                count += prefix_sum[target]
+            prefix_sum[curr] = prefix_sum.get(curr, 0) + 1
         return count
