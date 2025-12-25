4260701e59bba7dd632ca08d8623719be076c965
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-25 09:47:50 +0100

    Time: 31 ms (99.20%) | Memory: 17.7 MB (98.33%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,38 +1,40 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         def check(n):
-            mod = 10**9 + 7
-            base = 101
+            hash1, hash2 = 0, 0
+            base, mod = 101, 10**9 + 7
             highest_pow = pow(base, n - 1, mod)
-            hash1 = 0
-            hash2 = 0
             seen = set()
+            # calc hash of the first window of nums1
             for i in range(n):
                 hash1 = (hash1 * base + nums1[i]) % mod
             seen.add(hash1)
+            # calc the hashs of the rest of nums1
             for i in range(1, len(nums1) - n + 1):
-                old = nums1[i - 1] * highest_pow % mod
-                new = nums1[i + n - 1] % mod
+                new = nums1[i + n - 1]
+                old = (nums1[i - 1] * highest_pow) % mod
                 hash1 = ((hash1 - old) * base + new) % mod
                 seen.add(hash1)
+            # calc hash of the first window of nums2
             for i in range(n):
                 hash2 = (hash2 * base + nums2[i]) % mod
             if hash2 in seen:
                 return True
             for i in range(1, len(nums2) - n + 1):
-                old = nums2[i - 1] * highest_pow % mod
-                new = nums2[i + n - 1] % mod
+                new = nums2[i + n - 1]
+                old = (nums2[i - 1] * highest_pow) % mod
                 hash2 = ((hash2 - old) * base + new) % mod
                 if hash2 in seen:
                     return True
             return False
-        low, high = 0, min(len(nums1), len(nums2))
+
+        l, r = 0, min(len(nums1), len(nums2))
         ans = 0
-        while low <= high:
-            mid = (low + high) // 2
+        while l <= r:
+            mid = (l + r) // 2
             if check(mid):
                 ans = mid
-                low = mid + 1
+                l = mid + 1
             else:
-                high = mid - 1
+                r = mid - 1
         return ans
