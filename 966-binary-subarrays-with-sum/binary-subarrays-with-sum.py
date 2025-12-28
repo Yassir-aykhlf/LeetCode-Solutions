2acbeadd3ab64e769cfe1306c4691edd86694a65
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-28 15:58:33 +0100

    Time: 27 ms (84.18%) | Memory: 18.6 MB (76.70%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,12 +1,16 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        prefix = {0: 1}
-        count = 0
-        curr = 0
-        for num in nums:
-            curr += num
-            tar = curr - goal
-            if tar in prefix:
-                count += prefix[tar]
-            prefix[curr] = prefix.get(curr, 0) + 1
-        return count
+        def atMostK(k):
+            if k < 0:
+                return 0
+            curr = 0
+            count = 0
+            l = 0
+            for r in range(len(nums)):
+                curr += nums[r]
+                while curr > k:
+                    curr -= nums[l]
+                    l += 1
+                count += r - l + 1
+            return count
+        return atMostK(goal) - atMostK(goal - 1)
