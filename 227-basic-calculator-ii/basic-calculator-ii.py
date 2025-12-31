d38a353a60d609f5adb3799e97978aa1f5acb795
yassir aykhlf <aykhlf15yassir@gmail.com>
2025-12-31 09:54:00 +0100

    Time: 63 ms (71.85%) | Memory: 20.4 MB (69.51%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,22 +1,20 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        _s = [c for c in s if c != ' ']
-        n = len(_s)
         stack = []
-        op = '+'
         num = 0
-        for i, c in enumerate(_s):
+        sign = '+'
+        for i, c in enumerate(s):
             if c.isdigit():
                 num = num * 10 + int(c)
-            if c in "+-*/" or i == n -1:
-                if op == '+':
+            if c in "+-*/" or i == len(s) - 1:
+                if sign == '+':
                     stack.append(num)
-                elif op == '-':
+                elif sign == '-':
                     stack.append(-num)
-                elif op == '*':
+                elif sign == '*':
                     stack.append(stack.pop() * num)
-                elif op == '/':
+                elif sign == '/':
                     stack.append(int(stack.pop() / num))
-                op = c
+                sign = c
                 num = 0
         return sum(stack)
