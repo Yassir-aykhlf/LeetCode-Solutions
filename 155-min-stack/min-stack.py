6a2f06329ad4237e8ef2cb5528a7491fe83ac68a
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-01 11:05:47 +0100

    Time: 6 ms (54.19%) | Memory: 20.6 MB (87.04%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,19 @@
+class MinStack:
+
+    def __init__(self):
+        self.vals = []
+        self.mins = []
+
+    def push(self, val: int) -> None:
+        self.vals.append(val)
+        self.mins.append(val if not self.mins else min(self.mins[-1], val))
+
+    def pop(self) -> None:
+        self.vals.pop()
+        self.mins.pop()
+
+    def top(self) -> int:
+        return self.vals[-1]
+
+    def getMin(self) -> int:
+        return self.mins[-1]
