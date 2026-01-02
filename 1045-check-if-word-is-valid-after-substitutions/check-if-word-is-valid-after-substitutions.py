67c9d7b3297467e795232149f0a0dcdf31857734
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-02 10:19:09 +0100

    Time: 64 ms (15.07%) | Memory: 17.6 MB (84.93%) - LeetSync

--- 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.py
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.py
@@ -1,10 +1,9 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) < 3:
-            return False
         stack = []
         for c in s:
             stack.append(c)
             if len(stack) >= 3 and stack[-3:] == ['a', 'b', 'c']:
-                stack = stack[:-3]
+                print(stack[-3:])
+                del stack[-3:]
         return not stack
