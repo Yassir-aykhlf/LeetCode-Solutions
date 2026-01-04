5931852d4bd7835a10cb73022e634855fd14b069
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-04 15:52:48 +0100

    Time: 31 ms (58.20%) | Memory: 18.7 MB (75.31%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        def atMostK(k):
+        def atMost(k):
             if k < 0:
                 return 0
-            curr = 0
             count = 0
+            curr = 0
             l = 0
             for r in range(len(nums)):
                 curr += nums[r]
@@ -13,4 +13,4 @@ class Solution:
                     l += 1
                 count += r - l + 1
             return count
-        return atMostK(goal) - atMostK(goal - 1)
+        return atMost(goal) - atMost(goal - 1)
