746407622f6a4e1b6163ce9bfc56a79277664a19
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-04 14:55:24 +0100

    Time: 0 ms (100.00%) | Memory: 17.4 MB (86.80%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.py
@@ -2,11 +2,10 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         stack = []
         for c in s:
-            if c == ')':
-                if stack and stack[-1] == '(':
-                    stack.pop()
-                else:
-                    stack.append(c)
+            if c == '(':
+                stack.append(c)
+            elif stack and stack[-1] == '(':
+                stack.pop()
             else:
                 stack.append(c)
         return len(stack)
