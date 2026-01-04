80f3f9c5b7b693a380cde4628a6cb3c493c344ef
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-04 15:42:59 +0100

    Time: 60 ms (93.89%) | Memory: 28.3 MB (96.28%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -1,21 +1,18 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         n = len(nums)
-        total_sum = sum(nums)
-        target = total_sum - x
-        if target == 0:
-            return n
-        if total_sum < x:
-            return -1
-        # looking for the max window with sum target
-        max_len = 0
+        num_sum = sum(nums)
+        tar_sum = num_sum - x
+        if num_sum < x: return -1
+        if num_sum == x: return n
+        max_del = 0
         curr = 0
         l = 0
-        for r in range(n):
+        for r in range(len(nums)):
             curr += nums[r]
-            while curr > target:
+            while curr > tar_sum:
                 curr -= nums[l]
                 l += 1
-            if curr == target:
-                max_len = max(max_len, r - l + 1)
-        return n - max_len if max_len else -1
+            if curr == tar_sum:
+                max_del = max(max_del, r - l + 1)
+        return n - max_del if max_del else -1
