4c9c744b92e3dcb7068e79fa0dd7c019cffc3106
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-04 15:18:37 +0100

    Time: 51 ms (57.26%) | Memory: 18.2 MB (93.74%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,29 +1,25 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
         res = 0
-        num = 0
         sign = 1
+        num = 0
+        stack = []
         for c in s:
             if c.isdigit():
                 num = num * 10 + int(c)
-            elif c == '+':
-                res += sign * num
+            elif c == '+' or c == '-':
+                res += num * sign
+                sign = 1 if c == '+' else -1
                 num = 0
-                sign = 1
-            elif c == '-':
-                res += sign * num
-                num = 0
-                sign = -1
             elif c == ')':
-                res += sign * num
-                num = 0
+                res += num * sign
                 prev_sign = stack.pop()
                 prev_res = stack.pop()
-                res = prev_res + res * prev_sign
+                res = prev_res + (prev_sign * res)
+                num = 0
             elif c == '(':
                 stack.append(res)
                 stack.append(sign)
-                res = 0
                 sign = 1
+                res = 0
         return res + num * sign
