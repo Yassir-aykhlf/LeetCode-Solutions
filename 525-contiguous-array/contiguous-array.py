580435c7bb9076731cf36849e8e1976430471c70
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-04 13:51:59 +0100

    Time: 99 ms (76.25%) | Memory: 22.3 MB (88.73%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,12 +1,12 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        curr = 0
-        max_len = 0
         prefix = {0: -1}
-        for i, num in enumerate(nums):
-            curr += 1 if num else -1
+        max_len = 0
+        curr = 0
+        for r in range(len(nums)):
+            curr += 1 if nums[r] == 1 else -1
             if curr in prefix:
-                max_len = max(max_len, i - prefix[curr])
+                max_len = max(max_len, r - prefix[curr])
             else:
-                prefix[curr] = i
+                prefix[curr] = r
         return max_len
