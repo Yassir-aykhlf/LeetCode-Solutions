ccd2f362f3733a3938ffa07c9eec2e69cfabf2a3
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-05 11:09:47 +0100

    Time: 39 ms (54.63%) | Memory: 23.7 MB (91.17%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,24 +1,26 @@
 class Solution:
-    def maxHisto(self, nums):
+    def maxHistogram(self, arr):
         stack = []
         max_area = 0
-        for i, h in enumerate(nums):
+        for i, h in enumerate(arr):
             start = i
             while stack and stack[-1][1] > h:
-                index, height = stack.pop()
-                start = index
-                max_area = max(max_area, height * (i - index))
+                idx, hi = stack.pop()
+                max_area = max(max_area, hi * (i - idx))
+                start = idx
             stack.append((start, h))
         for i, h in stack:
-            max_area = max(max_area, h * (len(nums) - i))
+            max_area = max(max_area, h * (len(arr) - i))
         return max_area
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        m = len(matrix)
-        n = len(matrix[0])
-        cols = [0] * n
+        if not matrix or not matrix[0]:
+            return 0
         max_area = 0
-        for row in range(m):
-            for col in range(n):
-                cols[col] = cols[col] + 1 if matrix[row][col] != "0" else 0
-            max_area = max(max_area, self.maxHisto(cols))
+        n = len(matrix)
+        m = len(matrix[0])
+        cols = [0] * m
+        for row in range(n):
+            for col in range(m):
+                cols[col] = cols[col] + 1 if matrix[row][col] == "1" else 0
+            max_area = max(max_area, self.maxHistogram(cols))
         return max_area
