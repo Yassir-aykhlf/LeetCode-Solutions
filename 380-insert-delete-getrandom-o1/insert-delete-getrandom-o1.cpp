33d19b06e90dacdcacedefb3b978d80e446c9c7f
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-07 15:13:59 +0100

    Time: 37 ms (81.14%) | Memory: 113.1 MB (87.49%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,39 +1,33 @@
 class RandomizedSet {
 private:
-    std::vector<int> vals;
-    std::unordered_map<int, int> val_idx;
+    std::vector<int> _vec;
+    std::unordered_map<int, int> _val_idx;
 public:
-    RandomizedSet() {
-    }
+    RandomizedSet() {}
     
     bool insert(int val) {
-        if (val_idx.find(val) != val_idx.end()) {
+        if (_val_idx.find(val) != _val_idx.end()) {
             return false;
         }
-        vals.push_back(val);
-        val_idx[val] = vals.size() - 1;
+        _vec.push_back(val);
+        _val_idx[val] = _vec.size() - 1;
         return true;
     }
     
     bool remove(int val) {
-        if (val_idx.find(val) == val_idx.end()) {
+        if (_val_idx.find(val) == _val_idx.end()) {
             return false;
         }
-        int del_idx = val_idx[val];
-        int last_val = vals.back();
-        vals[del_idx] = last_val;
-        vals.pop_back();
-        val_idx[last_val] = del_idx;
-        val_idx.erase(val);
+        int tar_idx = _val_idx[val];
+        int las_val = _vec.back();
+        _vec[tar_idx] = las_val;
+        _val_idx[las_val] = tar_idx;
+        _vec.pop_back();
+        _val_idx.erase(val);
         return true;
     }
     
     int getRandom() {
-        return vals[std::rand() % vals.size()];
+        return _vec[std::rand() % _vec.size()];
     }
 };
-
-/**
-["RandomizedSet","insert","remove","insert","getRandom","remove","insert","getRandom"]
-[[],[1],[2],[2],[],[1],[2],[]]
- */
