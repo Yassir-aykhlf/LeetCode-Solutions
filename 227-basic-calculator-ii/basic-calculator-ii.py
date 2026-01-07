ed00dd3c23c0c039e7039063dfbefd6fbc633493
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-07 15:29:37 +0100

    Time: 35 ms (99.83%) | Memory: 22.6 MB (11.56%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.py
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -1,21 +1,21 @@
 class Solution:
     def calculate(self, s: str) -> int:
         stack = []
-        num = 0
-        last_sign = '+'
+        las_sig = '+'
         s += '+'
+        num = 0
         for c in s:
             if c.isdigit():
                 num = num * 10 + int(c)
             elif c in "+-*/":
-                if last_sign == '+':
+                if las_sig == '+':
                     stack.append(num)
-                if last_sign == '-':
+                elif las_sig == '-':
                     stack.append(-num)
-                elif last_sign == '*':
+                elif las_sig == '*':
                     stack.append(stack.pop() * num)
-                elif last_sign == '/':
+                elif las_sig == '/':
                     stack.append(int(stack.pop() / num))
-                last_sign = c
                 num = 0
+                las_sig = c
         return sum(stack)
