4d6ff886edb5ef7844fcb73cdf6f367eb958975f
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-12 10:58:00 +0100

    Time: 31 ms (98.47%) | Memory: 19.6 MB (77.81%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,38 +1,37 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         def check(n):
-            base, mod = 101, 10 ** 9 + 7
-            highest_pow = pow(base, n - 1, mod)
-            h1, h2 = 0, 0
             seen = set()
+            base = 101
+            mod = 10 ** 9 + 7
+            highest_pow = pow(base, n - 1, mod)
+            hash1 = 0
+            hash2 = 0
             for i in range(n):
-                h1 = (h1 * base + nums1[i]) % mod
-            seen.add(h1)
+                hash1 = (hash1 * base + nums1[i]) % mod
+            seen.add(hash1)
             for i in range(n, len(nums1)):
                 new = nums1[i]
                 old = nums1[i - n]
-                h1 = ((h1 - old * highest_pow) * base + new) % mod
-                seen.add(h1)
-            
+                hash1 = ((hash1 - old * highest_pow) * base + new) % mod
+                seen.add(hash1)
             for i in range(n):
-                h2 = (h2 * base + nums2[i]) % mod
-            if h2 in seen:
+                hash2 = (hash2 * base + nums2[i]) % mod
+            if hash2 in seen:
                 return True
             for i in range(n, len(nums2)):
                 new = nums2[i]
                 old = nums2[i - n]
-                h2 = ((h2 - old * highest_pow) * base + new) % mod
-                if h2 in seen:
+                hash2 = ((hash2 - old * highest_pow) * base + new) % mod
+                if hash2 in seen:
                     return True
             return False
 
         lo, hi = 0, min(len(nums1), len(nums2))
-        ans = 0
         while lo <= hi:
             n = (lo + hi) // 2
             if check(n):
-                ans = n
                 lo = n + 1
             else:
                 hi = n - 1
-        return ans
+        return hi
