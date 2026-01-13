ae5c3572e8a0ddb173834c8eab97bc7a8b2c64de
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-13 11:06:23 +0100

    Time: 27 ms (84.90%) | Memory: 24.8 MB (17.33%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,24 +1,24 @@
 class Solution:
+    def maxRec(self, heights: List) -> int:
+        res = 0
+        stack = []
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > h:
+                idx, hi = stack.pop()
+                res = max(res, hi * (i - idx))
+                start = idx
+            stack.append((start, h))
+        for i, h in stack:
+            res = max(res, h * (len(heights) - i))
+        return res
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        max_area = 0
         n = len(matrix)
         m = len(matrix[0])
         cols = [0] * m
-        max_area = 0
-        def maxRec(cols):
-            stack = []
-            max_area = 0
-            for i, c in enumerate(cols):
-                start = i
-                while stack and stack[-1][1] > c:
-                    idx, val = stack.pop()
-                    max_area = max(max_area, val * (i - idx))
-                    start = idx
-                stack.append((start, c))
-            for i, c in stack:
-                max_area = max(max_area, c * (len(cols) - i))
-            return max_area
         for row in range(n):
             for col in range(m):
                 cols[col] = cols[col] + 1 if matrix[row][col] == "1" else 0
-            max_area = max(max_area, maxRec(cols))
+            max_area = max(max_area, self.maxRec(cols))
         return max_area
