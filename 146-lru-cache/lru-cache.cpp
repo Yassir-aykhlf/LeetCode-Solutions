26a4f6202161348b47f938305eaa03bcd2e8e3fd
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-14 10:29:30 +0100

    Time: 64 ms (79.20%) | Memory: 173.1 MB (81.84%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,33 +1,33 @@
 class LRUCache {
 private:
-    const unsigned int capacity;
-    std::list<std::pair<int, int>> cacheList;
-    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> cacheMap;
+    int capacity;
+    std::list<std::pair<int, int>> cachelist;
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> cachemap;
 public:
     LRUCache(int capacity) : capacity(capacity) {}
     
     int get(int key) {
-        if (cacheMap.find(key) == cacheMap.end()) {
+        if (cachemap.find(key) == cachemap.end()) {
             return -1;
         }
-        cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
-        cacheMap[key] = cacheList.begin();
-        return cacheMap[key]->second;
+        cachelist.splice(cachelist.begin(), cachelist, cachemap[key]);
+        cachemap[key] = cachelist.begin();
+        return cachelist.begin()->second;
     }
     
     void put(int key, int value) {
-        if (cacheMap.find(key) != cacheMap.end()) {
-            cacheList.splice(cacheList.begin(), cacheList, cacheMap[key]);
-            cacheMap[key] = cacheList.begin();
-            cacheMap[key]->second = value;
+        if (cachemap.find(key) != cachemap.end()) {
+            cachelist.splice(cachelist.begin(), cachelist, cachemap[key]);
+            cachemap[key] = cachelist.begin();
+            cachelist.begin()->second = value;
             return;
         }
-        if (capacity == cacheList.size()) {
-            int last = cacheList.back().first;
-            cacheList.pop_back();
-            cacheMap.erase(last);
+        if (capacity == cachelist.size()) {
+            int del_key = cachelist.back().first;
+            cachelist.pop_back();
+            cachemap.erase(del_key);
         }
-        cacheList.push_front({key, value});
-        cacheMap[key] = cacheList.begin();
+        cachelist.push_front(std::make_pair(key, value));
+        cachemap[key] = cachelist.begin();
     }
 };
