275ff4d640055628c7131328e1ec4698cc5df9d9
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-16 11:40:49 +0100

    Time: 0 ms (100.00%) | Memory: 19.4 MB (10.72%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,15 +1,12 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         lo, hi = 0, x
-        best = 0
         while lo <= hi:
             mid = (lo + hi) // 2
-            res = mid * mid
-            if res == x:
-                return mid
-            elif res > x:
+            if mid * mid < x:
+                lo = mid + 1
+            elif mid * mid > x:
                 hi = mid - 1
             else:
-                best = mid
-                lo = mid + 1
-        return best
+                return mid
+        return hi
