d82b428c5bb65d7e401b3908c3cab1769d5fd3ff
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-17 11:52:38 +0100

    Time: 0 ms (100.00%) | Memory: 19.5 MB (18.90%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -3,10 +3,10 @@ class Solution:
         lo, hi = 0, len(nums) - 1
         while lo <= hi:
             mid = (lo + hi) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 lo = mid + 1
-            else:
+            elif nums[mid] > target:
                 hi = mid - 1
+            else:
+                return mid
         return lo
