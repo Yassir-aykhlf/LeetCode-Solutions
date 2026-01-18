4a3edc8305ee49e0afc73d796c0f7fdb1b71d52a
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-18 14:30:29 +0100

    Time: 182 ms (9.93%) | Memory: 22.2 MB (12.15%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,16 +1,16 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
         def atMost(k):
-            l = 0
-            counter = 0
             freq = collections.defaultdict(int)
+            count = 0
+            l = 0
             for r in range(len(nums)):
                 freq[nums[r]] += 1
-                while len(freq) > k:
+                while len(freq.keys()) > k:
                     freq[nums[l]] -= 1
                     if freq[nums[l]] == 0:
                         del freq[nums[l]]
                     l += 1
-                counter += r - l + 1
-            return counter
+                count += r - l + 1
+            return count
         return atMost(k) - atMost(k - 1)
