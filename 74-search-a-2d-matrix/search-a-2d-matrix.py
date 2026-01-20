5cd240e9c05fe13b86f8b95dcc2089b1c2a05cda
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-20 14:13:09 +0100

    Time: 3 ms (4.13%) | Memory: 19.6 MB (18.08%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,21 +1,37 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        lo, hi = 0, len(matrix) - 1
+        if not matrix or not matrix[0]:
+            return False
+
+        # lo, hi = 0, len(matrix) - 1
+        # while lo <= hi:
+        #     mid = (lo + hi) // 2
+        #     if matrix[mid][0] == target:
+        #         return True
+        #     elif matrix[mid][0] < target:
+        #         lo = mid + 1
+        #     else:
+        #         hi = mid - 1
+        # row = matrix[hi]
+        # lo, hi = 0, len(row) - 1
+        # while lo <= hi:
+        #     mid = (lo + hi) // 2
+        #     if row[mid] == target:
+        #         return True
+        #     elif row[mid] < target:
+        #         lo = mid + 1
+        #     else:
+        #         hi = mid - 1
+        # return False
+
+        m, n = len(matrix), len(matrix[0]) 
+        lo, hi = 0, m * n - 1
         while lo <= hi:
             mid = (lo + hi) // 2
-            if matrix[mid][0] == target:
+            mid_val = matrix[mid // n][mid % n]
+            if mid_val == target:
                 return True
-            elif matrix[mid][0] < target:
-                lo = mid + 1
-            else:
-                hi = mid - 1
-        row = matrix[hi]
-        lo, hi = 0, len(row) - 1
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if row[mid] == target:
-                return True
-            elif row[mid] < target:
+            elif mid_val < target:
                 lo = mid + 1
             else:
                 hi = mid - 1
