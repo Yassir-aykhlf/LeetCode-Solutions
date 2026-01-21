1a94536dc6693a4e564727b71a11d90c2db9880b
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-21 13:54:56 +0100

    Time: 2 ms (46.99%) | Memory: 19.5 MB (6.85%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -3,7 +3,7 @@ class Solution:
         def mutate(num):
             res = 0
             while num > 0:
-                res += (num % 10) * (num % 10)
+                res += (num % 10) ** 2
                 num //= 10
             return res
         slow = fast = n
@@ -11,4 +11,8 @@ class Solution:
             slow = mutate(slow)
             fast = mutate(mutate(fast))
             if slow == fast:
+                slow = n
+                while slow != fast:
+                    slow = mutate(slow)
+                    fast = mutate(fast)
                 return slow == 1
