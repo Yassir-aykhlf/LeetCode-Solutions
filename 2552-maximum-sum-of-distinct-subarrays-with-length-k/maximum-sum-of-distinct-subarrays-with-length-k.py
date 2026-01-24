39e9d89eedcb178aa25a375290ca69015771ddef
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-24 17:50:50 +0100

    Time: 178 ms (7.38%) | Memory: 38.2 MB (5.45%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,16 +1,15 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        window = collections.Counter(nums[:k])
-        cur_sum = sum(nums[:k])
-        max_sum = cur_sum if len(window) == k else 0
-        n = len(nums)
-        for r in range(k, n):
-            cur_sum += nums[r]
-            cur_sum -= nums[r - k]
-            window[nums[r]] += 1
-            window[nums[r - k]] -= 1
-            if window[nums[r - k]] == 0:
-                del window[nums[r - k]]
-            if len(window) == k:
-                max_sum = max(max_sum, cur_sum)
+        freq = Counter(nums[:k])
+        curr_sum = sum(nums[:k])
+        max_sum = curr_sum if len(freq) == k else 0
+        for r in range(k, len(nums)):
+            curr_sum += nums[r]
+            curr_sum -= nums[r - k]
+            freq[nums[r]] += 1
+            freq[nums[r - k]] -= 1
+            if freq[nums[r - k]] == 0:
+                del freq[nums[r - k]]
+            if len(freq) == k:
+                max_sum = max(max_sum, curr_sum)
         return max_sum
