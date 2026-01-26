7390b5c37b01aade813da7cd7d52c288bedeedfd
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-26 09:37:20 +0100

    Time: 23 ms (55.03%) | Memory: 70.5 MB (45.47%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool    check(vector<int>& bloomDay, int m, int k, int day) {
         int flowers = 0, bouquets = 0;
-        for (auto b : bloomDay) {
+        for (int b : bloomDay) {
             if (b <= day) {
                 flowers += 1;
                 if (flowers == k) {
@@ -17,12 +17,15 @@ public:
         return bouquets >= m;
     }
     int     minDays(vector<int>& bloomDay, int m, int k) {
+        if ((long long)m * k > bloomDay.size()) {
+            return -1;
+        }
         int res = -1;
         int low = *std::min_element(bloomDay.begin(), bloomDay.end());
         int high = *std::max_element(bloomDay.begin(), bloomDay.end());
         int mid; 
         while (low <= high) {
-            mid = low + (high - low) / 2;
+            mid = (low + high) / 2;
             if (check(bloomDay, m, k, mid)) {
                 res = mid;
                 high = mid - 1;
