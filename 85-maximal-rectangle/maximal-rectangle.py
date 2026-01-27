18b139f588f8e75a5ce5fdd468dfd37e9ded3d7f
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-27 18:21:39 +0100

    Time: 34 ms (45.57%) | Memory: 24.6 MB (41.34%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,24 +1,25 @@
 class Solution:
-    def maxRec(self, nums: List[int]) -> int:
-        max_area = 0
-        stack = []
-        for i, num in enumerate(nums):
-            start = i
-            while stack and stack[-1][1] > num:
-                idx, pre = stack.pop()
-                max_area = max(max_area, pre * (i - idx))
-                start = idx
-            stack.append((start, num))
-        for i, num in stack:
-            max_area = max(max_area, num * (len(nums) - i))
-        return max_area
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        n = len(matrix)
-        m = len(matrix[0])
-        cols = [0] * m
+        if not matrix or not matrix[0]:
+            return 0
+        def maxHisto(cols):
+            max_area = 0
+            stack = []
+            for i, h in enumerate(cols):
+                start = i
+                while stack and stack[-1][1] > h:
+                    idx, hi = stack.pop()
+                    max_area = max(max_area, hi * (i - idx))
+                    start = idx
+                stack.append((start, h))
+            for i, h in stack:
+                max_area = max(max_area, h * (len(cols) - i))
+            return max_area
+        m, n = len(matrix), len(matrix[0])
+        cols = [0] * n
         max_area = 0
-        for row in range(n):
-            for col in range(m):
+        for row in range(m):
+            for col in range(n):
                 cols[col] = cols[col] + 1 if matrix[row][col] == "1" else 0
-            max_area = max(max_area, self.maxRec(cols))
+            max_area = max(max_area, maxHisto(cols))
         return max_area
