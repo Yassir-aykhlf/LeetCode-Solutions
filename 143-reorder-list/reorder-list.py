e21e6405812cda8768dd77a386b334b40200c837
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-27 18:00:58 +0100

    Time: 1 ms (61.11%) | Memory: 27.9 MB (28.27%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -9,16 +9,18 @@ class Solution:
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        curr = slow
-        prev = None
-        while curr:
-            curr_next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = curr_next
+        mid = slow
+        prev= None
+        while mid:
+            mid_nxt = mid.next
+            mid.next = prev
+            prev = mid
+            mid = mid_nxt
         r1, r2 = head, prev
         while r2.next:
-            r1_next, r2_next = r1.next, r2.next
+            r1_next = r1.next
+            r2_next = r2.next
             r1.next = r2
             r2.next = r1_next
-            r1, r2 = r1_next, r2_next
+            r1 = r1_next
+            r2 = r2_next
