f26adc769690751ba7789014089fa81a42faafb8
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-28 16:47:50 +0100

    Time: 5 ms (21.19%) | Memory: 19.7 MB (17.58%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,4 +1,24 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        comb = sorted(nums1 + nums2)
-        return comb[len(comb) // 2] if len(comb) % 2 == 1 else sum(comb[len(comb) // 2 - 1 : len(comb) // 2 + 1]) / 2
+        A, B = nums1, nums2
+        if len(B) < len(A):
+            A, B = B, A
+        A_len, B_len = len(A), len(B)
+        total = A_len + B_len
+        half = total // 2
+        lo, hi = 0, A_len - 1
+        while True:
+            i = (lo + hi) // 2
+            j = half - (i + 1) - 1
+            Aleft = A[i] if i >= 0 else float('-inf')
+            Aright = A[i + 1] if i + 1 < A_len else float('+inf')
+            Bleft = B[j] if j >= 0 else float('-inf')
+            Bright = B[j + 1] if j + 1 < B_len else float('+inf')
+            if Aleft <= Bright and Bleft <= Aright:
+                if total % 2:
+                    return min(Aright, Bright)
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                hi = i - 1
+            elif Bleft > Aright:
+                lo = i + 1
