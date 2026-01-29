1179edfb39dddd5ef2e94eaa2fb365cb6940b0e9
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-01-29 21:54:31 +0100

    Time: 116 ms (88.96%) | Memory: 55.8 MB (6.01%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        heights = [h for _, h in envelopes]
+        LIS = []
+        for h in heights:
+            idx = bisect.bisect_left(LIS, h)
+            if idx == len(LIS):
+                LIS.append(h)
+            else:
+                LIS[idx] = h
+        return len(LIS)
