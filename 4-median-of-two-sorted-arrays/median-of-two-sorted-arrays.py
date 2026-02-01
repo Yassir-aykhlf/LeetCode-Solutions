a708402b2b7e282acc20c845a82f14e2e4ceb0ea
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-01 15:15:55 +0100

    Time: 3 ms (50.26%) | Memory: 19.6 MB (20.95%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,24 +1,24 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A, B = nums1, nums2
-        if len(B) < len(A):
+        if len(nums1) > len(nums2):
             A, B = B, A
         A_len, B_len = len(A), len(B)
-        total_len = A_len + B_len
-        half = total_len // 2
+        total = A_len + B_len
+        half = total // 2
         lo, hi = 0, A_len - 1
         while True:
             i = (lo + hi) // 2
             j = half - (i + 1) - 1
-            Aleft = A[i] if i >= 0 else float('-inf')
-            Aright = A[i + 1] if i + 1 < A_len else float('inf')
-            Bleft = B[j] if j >= 0 else float('-inf')
-            Bright = B[j + 1] if j + 1 < B_len else float('inf')
-            if Aleft <= Bright and Bleft <= Aright:
-                if total_len % 2:
-                    return min(Aright, Bright)
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:
+            A_left = A[i] if i >= 0 else float('-inf')
+            A_right = A[i + 1] if i + 1 < A_len else float('inf')
+            B_left = B[j] if j >= 0 else float('-inf')
+            B_right = B[j + 1] if j + 1 < B_len else float('inf')
+            if A_left <= B_right and B_left <= A_right:
+                if total % 2:
+                    return min(A_right, B_right)
+                return (max(A_left, B_left) + min(A_right, B_right)) / 2
+            if A_left > B_right:
                 hi = i - 1
-            elif Bleft > Aright:
+            else:
                 lo = i + 1
