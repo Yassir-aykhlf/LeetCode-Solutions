e72fea31c326811167db9a4aaabf84f729acd649
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-02 20:38:39 +0100

    Time: 27 ms (99.26%) | Memory: 19.4 MB (84.00%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -1,32 +1,38 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        def search(n):
-            base = 101
-            mod = 10 ** 9 + 7
-            highest_pow = pow(base, n - 1, mod)
+        def check(n):
             seen = set()
-            nums1_hash = 0
-            nums2_hash = 0
+            MOD = 10 ** 9 + 7
+            BASE = 101
+            highest_pow = pow(BASE, n - 1, MOD)
+            hash1 = 0
             for i in range(n):
-                nums1_hash = (nums1_hash * base + nums1[i]) % mod
-            seen.add(nums1_hash)
+                hash1 = (hash1 * BASE + nums1[i]) % MOD
+            seen.add(hash1)
             for i in range(n, len(nums1)):
-                nums1_hash = ((nums1_hash - nums1[i - n] * highest_pow) * base + nums1[i]) % mod
-                seen.add(nums1_hash)
+                new = nums1[i]
+                old = nums1[i - n]
+                hash1 = ((hash1 - old * highest_pow) * BASE + new) % MOD
+                seen.add(hash1)
+            hash2 = 0
             for i in range(n):
-                nums2_hash = (nums2_hash * base + nums2[i]) % mod
-            if nums2_hash in seen:
+                hash2 = (hash2 * BASE + nums2[i]) % MOD
+            if hash2 in seen:
                 return True
             for i in range(n, len(nums2)):
-                nums2_hash = ((nums2_hash - nums2[i - n] * highest_pow) * base + nums2[i]) % mod
-                if nums2_hash in seen:
+                new = nums2[i]
+                old = nums2[i - n]
+                hash2 = ((hash2 - old * highest_pow) * BASE + new) % MOD
+                if hash2 in seen:
                     return True
             return False
         lo, hi = 0, min(len(nums1), len(nums2))
+        ans = hi
         while lo <= hi:
             mid = (lo + hi) // 2
-            if search(mid):
+            if check(mid):
+                ans = mid
                 lo = mid + 1
             else:
                 hi = mid - 1
-        return hi
+        return ans
