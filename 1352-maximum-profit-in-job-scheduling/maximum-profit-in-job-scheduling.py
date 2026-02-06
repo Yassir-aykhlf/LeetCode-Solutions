d96f6da36c0c412e825a7b6fe2927ecef56c3b7e
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-06 22:32:10 +0100

    Time: 67 ms (99.53%) | Memory: 32.7 MB (76.84%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,13 +1,12 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         jobs = sorted(zip(endTime, startTime, profit))
-        dp_end_times = [0]
-        dp_profits = [0]
+        dp_endtime = [0]
+        dp_profit = [0]
         for e, s, p in jobs:
-            idx = bisect.bisect_right(dp_end_times, s) - 1
-            # max profit just before current job
-            current_profit = p + dp_profits[idx]
-            if current_profit > dp_profits[-1]:
-                dp_end_times.append(e)
-                dp_profits.append(current_profit)
-        return dp_profits[-1]
+            idx = bisect.bisect_right(dp_endtime, s) - 1
+            current_profit = dp_profit[idx] + p
+            if current_profit > dp_profit[-1]:
+                dp_profit.append(current_profit)
+                dp_endtime.append(e)
+        return dp_profit[-1]
