7e035da13253b492969101b82a0e4411fbaf456e
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-07 18:06:07 +0100

    Time: 39 ms (10.93%) | Memory: 26.2 MB (5.45%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -8,20 +8,21 @@ You must find a solution with a memory complexity better than O(n^2).
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        n = len(matrix)
-        lo, hi = matrix[0][0], matrix[n - 1][n - 1]
-        while lo < hi:
-            mid = (lo + hi) // 2
-            count = 0
-            row, col = 0, n - 1
-            while row < n and col >= 0:
-                if matrix[row][col] <= mid:
-                    count += col + 1
-                    row += 1
-                else:
-                    col -= 1
-            if count >= k:
-                hi = mid
-            else:
-                lo = mid + 1
-        return hi
+        return heapq.nsmallest(k, [num for arr in matrix for num in arr])[-1]
+        # n = len(matrix)
+        # lo, hi = matrix[0][0], matrix[n - 1][n - 1]
+        # while lo < hi:
+        #     mid = (lo + hi) // 2
+        #     count = 0
+        #     row, col = 0, n - 1
+        #     while row < n and col >= 0:
+        #         if matrix[row][col] <= mid:
+        #             count += col + 1
+        #             row += 1
+        #         else:
+        #             col -= 1
+        #     if count >= k:
+        #         hi = mid
+        #     else:
+        #         lo = mid + 1
+        # return hi
