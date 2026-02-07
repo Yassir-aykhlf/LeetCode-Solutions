bfb1d912fb36ca5527a45f1b0c63c4967478480f
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-07 11:36:29 +0100

    Time: 0 ms (100.00%) | Memory: 22.9 MB (51.04%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,5 +1,5 @@
 class Solution:
+    """ Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order. """
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = Counter(nums)
-        # return sorted(freq.keys(), key= lambda x: freq[x], reverse=True)[ : k]
-        return heapq.nlargest(k, freq.keys(), key=lambda num: freq[num])
+        freq = collections.Counter(nums)
+        return heapq.nlargest(k, freq.keys(), key=lambda x: freq[x])
