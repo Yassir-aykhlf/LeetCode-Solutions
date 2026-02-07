bebdb9a8ca7704fc89e7725408affd9012b993b1
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-07 12:32:37 +0100

    Time: 66 ms (99.60%) | Memory: 33.7 MB (59.00%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.py
@@ -1,12 +1,18 @@
 class Solution:
+    """
+    We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+    You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time
+    range.
+    If you choose a job that ends at time X you will be able to start another job that starts at time X.
+    """
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         jobs = sorted(zip(endTime, startTime, profit))
-        dp_endtime = [0]
+        dp_endtimes = [0]
         dp_profit = [0]
-        for e, s, p in jobs:
-            idx = bisect.bisect_right(dp_endtime, s) - 1
-            current_profit = dp_profit[idx] + p
+        for end_time, start_time, profit in jobs:
+            last_job_idx = bisect.bisect_right(dp_endtimes, start_time) - 1
+            current_profit = profit + dp_profit[last_job_idx]
             if current_profit > dp_profit[-1]:
+                dp_endtimes.append(end_time)
                 dp_profit.append(current_profit)
-                dp_endtime.append(e)
         return dp_profit[-1]
