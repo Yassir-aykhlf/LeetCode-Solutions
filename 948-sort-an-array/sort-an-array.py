54eb3bde0c0ea53c72d894a1d52c6fd31f2f6f33
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-07 12:01:14 +0100

    Time: 565 ms (69.37%) | Memory: 27.2 MB (46.51%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,30 +1,35 @@
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        def mergeArr(arr, l, r):
-            sortd = []
-            i = l
-            mid = (l + r) // 2
-            j = mid + 1
-            while i <= mid and j <= r:
-                if arr[i] < arr[j]:
-                    sortd.append(arr[i])
-                    i += 1
-                else:
-                    sortd.append(arr[j])
-                    j += 1
-            while i <= mid:
-                sortd.append(arr[i])
+    """
+    Given an array of integers nums, sort the array in ascending order and return it.
+    You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+    """
+    def mergeArr(self, arr, l, r):
+        tmp = []
+        mid = (l + r) // 2
+        i = l
+        j = mid
+        while i < mid and j < r:
+            if arr[i] < arr[j]:
+                tmp.append(arr[i])
                 i += 1
-            while j <= r:
-                sortd.append(arr[j])
+            else:
+                tmp.append(arr[j])
                 j += 1
-            arr[l: r + 1] = sortd
-        def mergeSort(arr, l, r):
-            if l >= r:
-                return
-            mid = (l + r) // 2
-            mergeSort(arr, l, mid)
-            mergeSort(arr, mid + 1, r)
-            mergeArr(arr, l, r)
-        mergeSort(nums, 0, len(nums) - 1)
+        while i < mid:
+            tmp.append(arr[i])
+            i += 1
+        while j < r:
+            tmp.append(arr[j])
+            j += 1
+        arr[l : r] = tmp
+    def MergeSort(self, arr, l, r):
+        if r - 1 <= l:
+            return
+        mid = (l + r) // 2
+        self.MergeSort(arr, l, mid)
+        self.MergeSort(arr, mid, r)
+        self.mergeArr(arr, l, r)
+    def sortArray(self, nums: List[int]) -> List[int]:
+        l, r = 0, len(nums)
+        self.MergeSort(nums, l, r)
         return nums
