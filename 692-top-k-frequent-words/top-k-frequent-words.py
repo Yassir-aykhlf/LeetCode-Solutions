cda32f78c3761f7b139e14c8de2e2ce4fbe093ec
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-08 13:31:47 +0100

    Time: 0 ms (100.00%) | Memory: 19.6 MB (29.56%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,8 +1,4 @@
 class Solution:
-    """
-    Given an array of strings words and an integer k, return the k most frequent strings.
-    Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
-    """
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        freq = collections.Counter(words)
-        return heapq.nsmallest(k, freq.keys(), key=lambda x: (-freq[x], x))
+        freq = Counter(words)
+        return sorted(freq.keys(), key=lambda word: (-freq[word], word))[:k]
