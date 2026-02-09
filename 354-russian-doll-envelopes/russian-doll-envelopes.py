25fbba339f6f1b5546be3b97449a6e77ab77fcbb
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-09 16:29:55 +0100

    Time: 180 ms (30.12%) | Memory: 54.4 MB (43.47%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.py
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -1,12 +1,26 @@
 class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        """
+        Longest Increasing Subsequence
+        for Heights, because Widths are
+        garenteed to be sorted, the issue
+        and tie breaker is the Height
+        the LIS of Heights is the longest
+        possible increasing chain: LIS
+        How to implement LIS?
+        [ ] -> keep adding best canditates
+        best canditates: smaller than the largest
+        Binary Search? why? Bisect right or left?
+        """
         envelopes.sort(key=lambda x: (x[0], -x[1]))
+        print(envelopes)
         heights = [h for _, h in envelopes]
         LIS = []
         for h in heights:
-            idx = bisect.bisect_left(LIS, h)
-            if idx == len(LIS):
+            # bisect right to append to the seq?
+            prev_h_idx = bisect.bisect_left(LIS, h)
+            if prev_h_idx == len(LIS):
                 LIS.append(h)
             else:
-                LIS[idx] = h
+                LIS[prev_h_idx] = h
         return len(LIS)
