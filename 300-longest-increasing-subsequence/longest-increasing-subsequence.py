2c2dc6099e23951c5a881b36721da8215cf91292
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-09 17:02:16 +0100

    Time: 4 ms (87.64%) | Memory: 19.5 MB (53.21%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,10 +1,16 @@
+"""
+Input: [10,9,2,5,3,7,101,18]
+state: []
+[10]->[9]->[2]->[2,5]->[2,3]->[2,3,7]->[2,3,7,101]->[2,3,7,18]
+LIS len = len(state)
+"""
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        LIS = []
+        state = []
         for n in nums:
-            idx = bisect.bisect_left(LIS, n)
-            if idx == len(LIS):
-                LIS.append(n)
+            idx = bisect.bisect_left(state, n)
+            if idx == len(state):
+                state.append(n)
             else:
-                LIS[idx] = n
-        return len(LIS)
+                state[idx] = n
+        return len(state)
