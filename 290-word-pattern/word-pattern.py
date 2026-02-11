4f7e503073562d99c9af05a37393f95ad1f2dcd6
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-11 16:45:19 +0100

    Time: 0 ms (100.00%) | Memory: 19.3 MB (44.32%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -1,14 +1,15 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        words = s.split()
         chars = list(pattern)
+        words = s.split()
         if len(chars) != len(words):
             return False
-        w_c = {}
-        c_w = {}
+        char_word = {}
+        word_char = {}
         for c, w in zip(chars, words):
-            if (c in c_w and c_w[c] != w) or (w in w_c and w_c[w] != c):
-                return False
-            w_c[w] = c
-            c_w[c] = w
+            if c in char_word and char_word[c] != w or \
+               w in word_char and word_char[w] != c:
+               return False
+            char_word[c] = w
+            word_char[w] = c
         return True
