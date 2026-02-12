6ff8a05bd948305d4b8ab7ec5bfd52bce04d7841
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-12 15:31:49 +0100

    Time: 3 ms (89.47%) | Memory: 20.6 MB (49.00%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -1,10 +1,13 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
+        # match numbers to achieve k
+        # hashmap for fast lookup
+        # 0 k edge case
         freq = Counter(nums)
         count = 0
-        for num in freq:
+        for num in freq.keys():
             if k == 0:
-                if freq[num] > 1:
+                if num in freq and freq[num] > 1:
                     count += 1
             else:
                 if num + k in freq:
