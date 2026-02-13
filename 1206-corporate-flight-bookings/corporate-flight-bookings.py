ef810a64492deccae8190073dceea30df1e8d810
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-13 15:48:01 +0100

    Time: 24 ms (61.35%) | Memory: 30.7 MB (62.81%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.py
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -1,12 +1,12 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        events = [0] * (n + 2)
+        state = [0] * (n + 1)
         for first, last, seats in bookings:
-            events[first] += seats
-            events[last + 1] -= seats
+            state[first - 1] += seats
+            state[last] -= seats
         res = []
-        current_seats = 0
-        for seats in events[1:]:
-            current_seats += seats
-            res.append(current_seats)
-        return res[:-1]
+        current = 0
+        for seats in state[:-1]:
+            current += seats
+            res.append(current)
+        return res
