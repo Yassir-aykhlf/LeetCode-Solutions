519d708020439575d8e1d8efcf3af83ff19001cf
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-13 16:12:34 +0100

    Time: 60 ms (92.35%) | Memory: 69.2 MB (69.11%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,12 +1,3 @@
-"""
-Design a time-based key-value data structure that can store multiple values for the same key at different time stamps and retrieve the key's value at a certain timestamp.
-
-Implement the TimeMap class:
-
-TimeMap() Initializes the object of the data structure.
-void set(String key, String value, int timestamp) Stores the key key with the value value at the given time timestamp.
-String get(String key, int timestamp) Returns a value such that set was called previously, with timestamp_prev <= timestamp. If there are multiple such values, it returns the value associated with the largest timestamp_prev. If there are no values, it returns "".
-"""
 class TimeMap:
 
     def __init__(self):
@@ -18,13 +9,12 @@ class TimeMap:
         self.store[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        if key in self.store:
-            items = self.store[key]
-            idx = bisect.bisect_right(items, (timestamp, chr(127))) - 1
-            if idx < 0:
-                return ""
-            return items[idx][1]
-        return ""
+        if key not in self.store:
+            return ''
+        idx = bisect.bisect_right(self.store[key], (timestamp, chr(127))) - 1
+        if idx < 0:
+            return ''
+        return self.store[key][idx][1]
 
 
 # Your TimeMap object will be instantiated and called as such:
