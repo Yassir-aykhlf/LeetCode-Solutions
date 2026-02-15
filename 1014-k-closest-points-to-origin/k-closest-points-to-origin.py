97d70d73436a97f4f01a7185993aceed5cb246c8
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-15 13:56:04 +0000

    Time: 48 ms (87.09%) | Memory: 24.7 MB (42.07%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,3 +1,3 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        return heapq.nsmallest(k, points, key=lambda pt: pt[0]*pt[0] + pt[1]*pt[1])
+        return heapq.nsmallest(k, points, key = lambda pt: pt[0]**2 + pt[1]**2)
