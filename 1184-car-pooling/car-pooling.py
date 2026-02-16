175e66e57a9c30ede5ff84bad068c4df400e60a2
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-16 13:58:21 +0000

    Time: 0 ms (100.00%) | Memory: 19.9 MB (21.95%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,14 +1,13 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        # events: list[tuples(location, operation, value)]
         events = []
         for count, start, end in trips:
             events.append((start, 1, count))
             events.append((end, 0, -count))
         events.sort()
-        on_board = 0
-        for location, _, value in events:
-            on_board += value
-            if on_board > capacity:
+        load = 0
+        for point, _, value in events:
+            load += value
+            if load > capacity:
                 return False
         return True
