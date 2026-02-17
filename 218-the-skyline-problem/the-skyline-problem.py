c438448c935203826a9f3e78672959eecbad8cfe
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-17 17:04:47 +0000

    Time: 1998 ms (7.57%) | Memory: 26.7 MB (42.60%) - LeetSync

--- 218-the-skyline-problem/the-skyline-problem.py
+++ 218-the-skyline-problem/the-skyline-problem.py
@@ -5,24 +5,22 @@ class Solution:
             events.append((left, -height))
             events.append((right, height))
         events.sort()
+
         result = []
-        live_buildings = [0]
-        past_buildings = {}
+        heap = [0]
         prev_max_height = 0
+
         for x, h in events:
             if h < 0:
-                heapq.heappush(live_buildings, h)
+                heapq.heappush(heap, h)
             else:
-                val_to_remove = -h
-                past_buildings[val_to_remove] = past_buildings.get(val_to_remove, 0) + 1
-            while live_buildings[0] in past_buildings:
-                top = live_buildings[0]
-                heapq.heappop(live_buildings)
-                past_buildings[top] -= 1
-                if past_buildings[top] == 0:
-                    del past_buildings[top]
-            curr_max_height = -live_buildings[0]
+                heap.remove(-h)
+                heapq.heapify(heap)
+
+            curr_max_height = -heap[0]
+            
             if curr_max_height != prev_max_height:
                 result.append([x, curr_max_height])
                 prev_max_height = curr_max_height
+
         return result
