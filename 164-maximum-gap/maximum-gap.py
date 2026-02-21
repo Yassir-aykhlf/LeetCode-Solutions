74894cfe26a7ccd88ade2350d85585c6a6368f9f
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-21 15:57:05 +0000

    Time: 157 ms (55.45%) | Memory: 32 MB (46.15%) - LeetSync

--- 164-maximum-gap/maximum-gap.py
+++ 164-maximum-gap/maximum-gap.py
@@ -1,10 +1,10 @@
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
         nums.sort()
-        a, b = 0, 1
+        l, r = 0, 1
         max_gap = 0
-        while b < len(nums):
-            max_gap = max(max_gap, nums[b] - nums[a])
-            a += 1
-            b += 1
+        while r < len(nums):
+            max_gap = max(max_gap, nums[r] - nums[l])
+            l += 1
+            r += 1
         return max_gap
