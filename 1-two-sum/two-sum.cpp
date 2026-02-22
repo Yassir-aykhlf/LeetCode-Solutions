34972293dbdd37d389e5cc2b1f9122ccb74c4004
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-22 09:23:32 +0000

    twoSum
    Optimize twoSum function by reserving map size and improving lookup.

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         std::unordered_map<int, int> m;
+        m.reserve(nums.size());
         for (int i = 0; i < nums.size(); ++i) {
             int comp = target - nums[i];
-            if (m.find(comp) != m.end()) {
-                return {i, m[comp]};
+            auto it = m.find(comp);
+            if (it != m.end()) {
+                return {it->second, i};
             }
             m[nums[i]] = i;
         }
-        return {-1};
+        return {};
     }
 };
