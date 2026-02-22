449f51ed9e26dc9224142e154e17a30304927129
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-22 09:20:22 +0000

    Using map instead of unordered_map is just stupid :)

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::map<int, int> m;
+        std::unordered_map<int, int> m;
         for (int i = 0; i < nums.size(); ++i) {
             int comp = target - nums[i];
             if (m.find(comp) != m.end()) {
