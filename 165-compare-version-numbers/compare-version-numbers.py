743b4acd5803173e0404a862675beea0f29fa5b1
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-24 15:40:38 +0000

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.02%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,21 +1,21 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        nums1 = [int(num) for num in version1.split('.')]
-        nums2 = [int(num) for num in version2.split('.')]
+        v1 = [int(num) for num in version1.split('.')]
+        v2 = [int(num) for num in version2.split('.')]
         p1, p2 = 0, 0
-        while p1 < len(nums1) and p2 < len(nums2):
-            if nums1[p1] > nums2[p2]:
-                return 1
-            elif nums1[p1] < nums2[p2]:
+        while p1 < len(v1) and p2 < len(v2):
+            if v1[p1] < v2[p2]:
                 return -1
+            elif v1[p1] > v2[p2]:
+                return 1
             p1 += 1
             p2 += 1
-        while p1 < len(nums1):
-            if nums1[p1] > 0:
+        while p1 < len(v1):
+            if v1[p1] > 0:
                 return 1
             p1 += 1
-        while p2 < len(nums2):
-            if nums2[p2] > 0:
+        while p2 < len(v2):
+            if v2[p2] > 0:
                 return -1
             p2 += 1
         return 0
