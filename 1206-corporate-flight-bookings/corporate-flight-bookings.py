91ca8fb4fad725ada5a06f5e5399885e71f82988
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-25 03:27:02 +0000

    Time: 21 ms (83.33%) | Memory: 32.8 MB (5.65%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.py
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -1,20 +1,12 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        # O(N ^ 2) unacceptable for (2 * 10 ^ 4) size data
-        # answer = [0] * n
-        # for first, last, seats in bookings:
-        #     for i in range(first-1, last):
-        #         answer[i] += seats
-        # return answer
-
-        # O(N) solution, sweeping line
-        answer = [0] * n
-        events = [0] * (n + 2)
+        change = [0] * (n + 2)
         for first, last, seats in bookings:
-            events[first] += seats
-            events[last + 1] -= seats
-        current = 0
-        for i in range(n):
-            current += events[i+1]
-            answer[i] = current
-        return answer
+            change[first] += seats
+            change[last + 1] -= seats
+        load = 0
+        res = []
+        for i in change[1:]:
+            load += i
+            res.append(load)
+        return res[:-1]
