8ef5d042f9df908bfc254ad9c126269d25d14f3a
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-25 03:38:05 +0000

    Time: 71 ms (74.20%) | Memory: 21.8 MB (35.94%) - LeetSync

--- 1140-distant-barcodes/distant-barcodes.py
+++ 1140-distant-barcodes/distant-barcodes.py
@@ -1,14 +1,15 @@
 class Solution:
     def rearrangeBarcodes(self, barcodes: List[int]) -> List[int]:
         freq = Counter(barcodes)
-        max_heap = [(-count, num) for num, count in freq.items()]
+        max_heap = [(-count, code) for code, count in freq.items()]
         heapq.heapify(max_heap)
+        prev = None
         res = []
-        prev_count, prev_val = 0, 0
         while max_heap:
-            count, val = heapq.heappop(max_heap)
-            res.append(val)
-            if prev_count < 0:
-                heapq.heappush(max_heap, (prev_count, prev_val))
-            prev_count, prev_val = count + 1, val
+            count, code = heapq.heappop(max_heap)
+            res.append(code)
+            count += 1
+            if prev and prev[0] < 0:
+                heapq.heappush(max_heap, prev)
+            prev = (count, code)
         return res
