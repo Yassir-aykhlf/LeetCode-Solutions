6ce0ee9b3d6fe91e8f6d99a2e2142e6f49d313c6
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-02-25 04:24:16 +0000

    Time: 3 ms (4.14%) | Memory: 19.6 MB (33.98%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -5,14 +5,14 @@ class Solution:
             mid = (lo + hi) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] < nums[hi]:
-                if nums[mid] < target <= nums[hi]:
-                    lo = mid + 1
-                else:
-                    hi = mid - 1
-            else:
+            if nums[lo] <= nums[mid]:
                 if nums[lo] <= target < nums[mid]:
                     hi = mid - 1
                 else:
                     lo = mid + 1
+            else:
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
         return -1
