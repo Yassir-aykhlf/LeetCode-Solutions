bf8dcd7382f2de3db2d5d6236863999ee8d61df9
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-03-02 14:21:43 +0000

    Time: 1 ms (61.91%) | Memory: 20.4 MB (88.03%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,16 +6,16 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def check_depth(node):
+        def calcHeight(node: Optional[TreeNode]) -> int:
             if not node:
                 return 0
-            left_depth = check_depth(node.left)
-            if left_depth == -1:
+            left_height = calcHeight(node.left)
+            if left_height == -1:
                 return -1
-            right_depth = check_depth(node.right)
-            if right_depth == -1:
+            right_height = calcHeight(node.right)
+            if right_height == -1:
                 return -1
-            if abs(left_depth - right_depth) > 1:
+            if abs(left_height - right_height) > 1:
                 return -1
-            return max(left_depth, right_depth) + 1
-        return check_depth(root) != -1
+            return max(left_height, right_height) + 1
+        return calcHeight(root) != -1
