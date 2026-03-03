5559e0f0d8a18e6228bb401c172d7e2ec0bad3e4
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-03-03 22:06:03 +0000

    Time: 11 ms (25.40%) | Memory: 22.5 MB (67.29%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,17 +1,11 @@
-"""
-Given an array of intervals where intervals[i] = [starti, endi]
-merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
-"""
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals:
-            return []
         intervals.sort()
         result = [intervals[0]]
-        for current in intervals[1:]:
-            last_inter = result[-1]
-            if current[0] <= last_inter[1]:
-                last_inter[1] = max(current[1], last_inter[1])
+        for inter in intervals[1:]:
+            if inter[0] <= result[-1][1]:
+                result[-1][1] = max(result[-1][1], inter[1])
+                last_end = inter[1]
             else:
-                result.append(current)
+                result.append(inter)
         return result
