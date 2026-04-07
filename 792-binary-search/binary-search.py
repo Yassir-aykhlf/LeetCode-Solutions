c7949891fb9fc05a9c27f72b62a9c6df6723e0eb
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-07 10:49:38 +0100

    Time: 0 ms (100.00%) | Memory: 20.6 MB (52.52%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -5,8 +5,8 @@ class Solution:
             mid = (lo + hi) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                lo = mid + 1
-            else:
+            elif nums[mid] > target:
                 hi = mid - 1
-        return -1
+            else:
+                lo = mid + 1
+        return -1 
