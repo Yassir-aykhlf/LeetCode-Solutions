3343e6eb00d1e2432a5a611ffa01801e098e66fe
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-07 10:56:15 +0100

    Time: 0 ms (100.00%) | Memory: 31.3 MB (80.78%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -3,8 +3,8 @@ class Solution:
         lo, hi = 0, len(arr) - 1
         while lo < hi:
             mid = (lo + hi) // 2
-            if arr[mid] > arr[mid + 1]:
-                hi = mid
-            else:
+            if arr[mid] < arr[mid + 1]:
                 lo = mid + 1
+            else:
+                hi = mid
         return hi
