267fc31a3d1bd44e4e672a5bfa983c6a8db04446
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-13 21:11:21 +0100

    Time: 195 ms (11.01%) | Memory: 72.4 MB (5.01%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -6,29 +6,23 @@ class Solution:
         def dfs(row, col):
             if row < 0 or row >= m or \
                 col < 0 or col >= n or \
-                grid[row][col] == 0 or \
-                (row, col) in visited:
-                return
-            visited.add((row, col))
-            dfs(row + 1, col)
-            dfs(row - 1, col)
-            dfs(row, col + 1)
-            dfs(row, col - 1)
-        
+                (row, col) in visited or \
+                grid[row][col] == 0:
+                return 0
+            visited.add((row,col))
+            return 1 + dfs(row + 1, col) + dfs(row - 1, col) + dfs(row, col + 1) + dfs(row, col - 1)
         for row in range(m):
-            if grid[row][0] == 1 and (row, 0) not in visited:
+            if grid[row][0] == 1:
                 dfs(row, 0)
-            if grid[row][n - 1] == 1 and (row, n - 1) not in visited:
+            if grid[row][n - 1] == 1:
                 dfs(row, n - 1)
-
         for col in range(n):
-            if grid[0][col] == 1 and (0, col) not in visited:
+            if grid[0][col] == 1:
                 dfs(0, col)
-            if grid[m - 1][col] == 1 and (m - 1, col) not in visited:
+            if grid[m - 1][col] == 1:
                 dfs(m - 1, col)
-
         for row in range(1, m - 1):
             for col in range(1, n - 1):
                 if grid[row][col] == 1 and (row, col) not in visited:
-                    count += 1
+                    count += dfs(row, col)
         return count
