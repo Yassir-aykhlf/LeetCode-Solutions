1eb3f56234e15b30a945f978965e0f148c746b99
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-13 21:02:34 +0100

    Time: 15 ms (99.30%) | Memory: 22.2 MB (40.44%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,29 +1,25 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         m, n = len(heights), len(heights[0])
-        pacific_reachable = set()
-        atlantic_reachable = set()
-        def dfs(row, col, reachable_set, prev_height):
+        pacific_set = set()
+        atlantic_set = set()
+        def dfs(row, col, ocean_set, prev_height):
             if row < 0 or row >= m or \
                 col < 0 or col >= n or \
-                (row, col) in reachable_set or \
+                (row, col) in ocean_set or \
                 heights[row][col] < prev_height:
                 return
-            curr_height = heights[row][col]
-            reachable_set.add((row, col))
-            dfs(row + 1, col, reachable_set, curr_height)
-            dfs(row - 1, col, reachable_set, curr_height)
-            dfs(row, col + 1, reachable_set, curr_height)
-            dfs(row, col - 1, reachable_set, curr_height)
-            
-        for col in range(n):
-            dfs(0, col, pacific_reachable, heights[0][col])
+            ocean_set.add((row, col))
+            dfs(row + 1, col, ocean_set, heights[row][col])
+            dfs(row - 1, col, ocean_set, heights[row][col])
+            dfs(row, col + 1, ocean_set, heights[row][col])
+            dfs(row, col - 1, ocean_set, heights[row][col])
         for row in range(m):
-            dfs(row, 0, pacific_reachable, heights[row][0])
-
+            dfs(row, 0, pacific_set, heights[row][0])
         for col in range(n):
-            dfs(m - 1, col, atlantic_reachable, heights[m - 1][col])
+            dfs(0, col, pacific_set, heights[0][col])
         for row in range(m):
-            dfs(row, n - 1, atlantic_reachable, heights[row][n - 1])
-
-        return [list(coord) for coord in (pacific_reachable & atlantic_reachable)]
+            dfs(row, n - 1, atlantic_set, heights[row][n - 1])
+        for col in range(n):
+            dfs(m - 1, col, atlantic_set, heights[m - 1][col])
+        return [list(coord) for coord in (pacific_set & atlantic_set)]
