fc43eec9f806710bace341e1aef5fce3446b8637
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-14 09:25:20 +0100

    Time: 23 ms (89.05%) | Memory: 22.4 MB (27.78%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,8 +1,9 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        m, n = len(heights), len(heights[0])
         pacific_set = set()
         atlantic_set = set()
+        m, n = len(heights), len(heights[0])
+        
         def dfs(row, col, ocean_set, prev_height):
             if row < 0 or row >= m or \
                 col < 0 or col >= n or \
@@ -10,16 +11,16 @@ class Solution:
                 heights[row][col] < prev_height:
                 return
             ocean_set.add((row, col))
-            dfs(row + 1, col, ocean_set, heights[row][col])
+            dfs(row + 1, col, ocean_set, heights[row][col])            
             dfs(row - 1, col, ocean_set, heights[row][col])
             dfs(row, col + 1, ocean_set, heights[row][col])
             dfs(row, col - 1, ocean_set, heights[row][col])
-        for row in range(m):
+        
+        for row in range(0, m):
             dfs(row, 0, pacific_set, heights[row][0])
-        for col in range(n):
-            dfs(0, col, pacific_set, heights[0][col])
-        for row in range(m):
             dfs(row, n - 1, atlantic_set, heights[row][n - 1])
-        for col in range(n):
+        for col in range(0, n):
+            dfs(0, col, pacific_set, heights[0][col])
             dfs(m - 1, col, atlantic_set, heights[m - 1][col])
+
         return [list(coord) for coord in (pacific_set & atlantic_set)]
