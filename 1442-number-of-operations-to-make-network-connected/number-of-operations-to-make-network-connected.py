9d6f1d3844245781c7b0d09ebabfebd71960ae96
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-16 12:58:18 +0100

    Time: 34 ms (71.79%) | Memory: 33.5 MB (53.79%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -2,24 +2,20 @@ class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         if len(connections) < n - 1:
             return -1
-        computers = n
-        root_idx = [i for i in range(n)]
-
-        def find(node_idx):
-            if root_idx[node_idx] == node_idx:
-                return node_idx
-            root_idx[node_idx] = find(root_idx[node_idx])
-            return root_idx[node_idx]
-
-        def union(idx1, idx2):
-            root1 = find(idx1)
-            root2 = find(idx2)
-            if root1 == root2:
-                return False
-            root_idx[root1] = root2
-            return True
-
+        isolated = n
+        root = [i for i in range(n)]
+        def find(connection):
+            if root[connection] == connection:
+                return connection
+            root[connection] = find(root[connection])
+            return root[connection]
+        def union(end1, end2):
+            nonlocal isolated
+            root1 = find(end1)
+            root2 = find(end2)
+            if root1 != root2:
+                isolated -= 1
+                root[root1] = root2
         for u, v in connections:
-            if union(u, v):
-                computers -= 1
-        return computers -1
+            union(u, v)
+        return isolated - 1
