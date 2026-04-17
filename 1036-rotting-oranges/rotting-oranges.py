c97212b10784c5f7e154fd165ff297d3fada2bda
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-17 14:03:25 +0100

    Time: 3 ms (75.75%) | Memory: 19.4 MB (49.23%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,26 +1,29 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROW, COL = len(grid), len(grid[0])
         fresh, minutes = 0, 0
+        ROW, COL = len(grid), len(grid[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         dq = deque()
-        for r in range(ROW):
-            for c in range(COL):
-                if grid[r][c] == 1:
+        
+        for row in range(ROW):
+            for col in range(COL):
+                if grid[row][col] == 1:
                     fresh += 1
-                elif grid[r][c] == 2:
-                    dq.append((r,c))
-        while dq and fresh > 0:
+                elif grid[row][col] == 2:
+                    dq.append((row, col))
+
+        while dq and fresh:
             level_size = len(dq)
             for i in range(level_size):
                 rr, rc = dq.popleft()
                 for dr, dc in directions:
-                    row, col = rr + dr, rc + dc
-                    if 0 <= row < ROW and \
-                       0 <= col < COL and \
-                       grid[row][col] == 1:
-                       grid[row][col] = 2
-                       fresh -= 1
-                       dq.append((row, col))
+                    nr, nc = rr + dr, rc + dc
+                    if 0 <= nr < ROW and \
+                        0 <= nc < COL and \
+                        grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh -= 1
+                        dq.append((nr, nc))
             minutes += 1
-        return minutes if fresh == 0 else -1
+        
+        return minutes if not fresh else -1
