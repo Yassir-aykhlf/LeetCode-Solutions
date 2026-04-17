3175db2fe0db38c3905d728ae54288148ff25504
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-17 13:44:17 +0100

    Time: 35 ms (39.38%) | Memory: 22.3 MB (27.56%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,26 +1,26 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific_set = set()
-        atlantic_set = set()
-        m, n = len(heights), len(heights[0])
-        
-        def dfs(row, col, ocean_set, prev_height):
-            if row < 0 or row >= m or \
-                col < 0 or col >= n or \
-                (row, col) in ocean_set or \
+        p_ocean = set()
+        a_ocean = set()
+        ROW, COL = len(heights), len(heights[0])
+
+        def dfs(row, col, ocean, prev_height):
+            if row < 0 or row >= ROW or \
+                col < 0 or col >= COL or \
+                (row, col) in ocean or \
                 heights[row][col] < prev_height:
                 return
-            ocean_set.add((row, col))
-            dfs(row + 1, col, ocean_set, heights[row][col])            
-            dfs(row - 1, col, ocean_set, heights[row][col])
-            dfs(row, col + 1, ocean_set, heights[row][col])
-            dfs(row, col - 1, ocean_set, heights[row][col])
-        
-        for row in range(0, m):
-            dfs(row, 0, pacific_set, heights[row][0])
-            dfs(row, n - 1, atlantic_set, heights[row][n - 1])
-        for col in range(0, n):
-            dfs(0, col, pacific_set, heights[0][col])
-            dfs(m - 1, col, atlantic_set, heights[m - 1][col])
+            ocean.add((row, col))
+            dfs(row + 1, col, ocean, heights[row][col])
+            dfs(row - 1, col, ocean, heights[row][col])
+            dfs(row, col + 1, ocean, heights[row][col])
+            dfs(row, col - 1, ocean, heights[row][col])
 
-        return [list(coord) for coord in (pacific_set & atlantic_set)]
+        for row in range(ROW):
+            dfs(row, 0, p_ocean, heights[row][0])
+            dfs(row, COL - 1, a_ocean, heights[row][COL - 1])
+        for col in range(COL):
+            dfs(ROW - 1, col, a_ocean, heights[ROW - 1][col])
+            dfs(0, col, p_ocean, heights[0][col])
+        
+        return [list(coord) for coord in (p_ocean & a_ocean)]
