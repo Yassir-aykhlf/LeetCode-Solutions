7a9b1c355cb70c63d3d17e664325ee637d0c1d37
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-17 13:34:02 +0100

    Time: 86 ms (44.83%) | Memory: 43 MB (19.68%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -1,11 +1,12 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        visited = set()
         count = 0
+        visited = set()
+        ROW, COL = len(grid), len(grid[0])
+        
         def dfs(row, col):
-            if row < 0 or row >= m or \
-                col < 0 or col >= n or \
+            if row < 0 or row >= ROW or \
+                col < 0 or col >= COL or \
                 (row, col) in visited or \
                 grid[row][col] == 0:
                 return
@@ -14,18 +15,21 @@ class Solution:
             dfs(row - 1, col)
             dfs(row, col + 1)
             dfs(row, col - 1)
-        for row in range(m):
-            if grid[row][0] == 1:
+
+        for row in range(ROW):
+            if grid[row][0]:
                 dfs(row, 0)
-            if grid[row][n - 1] == 1:
-                dfs(row, n - 1)
-        for col in range(n):
-            if grid[0][col] == 1:
+            if grid[row][COL - 1]:
+                dfs(row, COL - 1)
+        for col in range(COL):
+            if grid[0][col]:
                 dfs(0, col)
-            if grid[m - 1][col] == 1:
-                dfs(m - 1, col)
-        for row in range(1, m - 1):
-            for col in range(1, n - 1):
-                if grid[row][col] == 1 and (row, col) not in visited:
+            if grid[ROW - 1][col]:
+                dfs(ROW - 1, col)
+        
+        for row in range(1, ROW - 1):
+            for col in range(1, COL - 1):
+                if grid[row][col] and (row, col) not in visited:
                     count += 1
+        
         return count
