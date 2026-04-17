06f55076e8424b5a874a02b298393e11d50703a8
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-17 14:31:44 +0100

    Time: 110 ms (82.46%) | Memory: 20 MB (89.69%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.py
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.py
@@ -1,28 +1,29 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
+        distance = 0
+        land = 0
+        dq = deque()
         ROW, COL = len(grid), len(grid[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        dq = deque()
-        distance = 0
-        water, land = 0, 0
-        for r in range(ROW):
-            for c in range(COL):
-                if grid[r][c] == 0:
-                    water += 1
+        for row in range(ROW):
+            for col in range(COL):
+                if grid[row][col] == 1:
+                    dq.append((row, col))
                 else:
-                    dq.append((r, c))
-        if len(dq) == 0 or len(dq) == ROW * COL:
+                    land += 1
+        if len(dq) == ROW * COL or len(dq) == 0:
             return -1
-        while dq:
+        while dq and land:
             level_size = len(dq)
             for i in range(level_size):
-                r, c = dq.popleft()
+                lr, lc = dq.popleft()
                 for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    if 0 <= row < ROW and \
-                       0 <= col < COL and \
-                       grid[row][col] == 0:
-                       grid[row][col] = 1
-                       dq.append((row, col))
+                    nr, nc = lr + dr, lc + dc
+                    if 0 <= nr < ROW and \
+                        0 <= nc < COL and \
+                        grid[nr][nc] == 0:
+                        grid[nr][nc] = 1
+                        dq.append((nr, nc))
+                        land -= 1
             distance += 1
-        return distance - 1
+        return distance
