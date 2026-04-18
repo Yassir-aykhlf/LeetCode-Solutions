25e82da273bcf17538a45283505c4d9b0aeb2cb8
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-18 09:13:29 +0100

    Time: 122 ms (85.33%) | Memory: 19.6 MB (98.96%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -3,24 +3,22 @@ class Solution:
         n = len(grid)
         if grid[0][0] == 1 or grid[n - 1][n - 1] == 1:
             return -1
-
         directions = [
             (-1, -1), (-1, 0), (-1, 1),
-            ( 0, -1)         , ( 0, 1),
-            ( 1, -1), ( 1, 0), ( 1, 1),
+            ( 0, -1),          ( 0, 1),
+            ( 1, -1), ( 1, 0), ( 1, 1)
         ]
-
         dq = deque([(0, 0, 1)])
         grid[0][0] = 1
         while dq:
-            row, col, length = dq.popleft()
-            if row == n - 1 and col == n - 1:
-                return length
+            r, c, distance = dq.popleft()
+            if r == n - 1 and c == n - 1:
+                return distance
             for dr, dc in directions:
-                nr, nc = row + dr, col + dc
+                nr, nc = r + dr, c + dc
                 if 0 <= nr < n and \
                     0 <= nc < n and \
                     grid[nr][nc] == 0:
                     grid[nr][nc] = 1
-                    dq.append((nr, nc, length + 1))
+                    dq.append((nr, nc, distance + 1))
         return -1
