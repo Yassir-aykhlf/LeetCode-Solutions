16e9cd54bcde57db1aabbe2365fed48fc335c5b3
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-19 21:07:18 +0100

    Time: 7 ms (50.97%) | Memory: 20.5 MB (48.29%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,26 +1,24 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
+        visited = set()
+        total = n
         root = [i for i in range(n)]
-        count = n
-
         def find(node):
-            if node == root[node]:
+            if root[node] == node:
                 return node
             root[node] = find(root[node])
             return root[node]
-
         def union(node1, node2):
-            nonlocal count
+            nonlocal total
             root1 = find(node1)
             root2 = find(node2)
             if root1 != root2:
                 root[root1] = root2
-                count -= 1
-
+                total -= 1
+                return
         for i in range(n):
             for j in range(i + 1, n):
-                if isConnected[i][j]:
+                if isConnected[i][j] == 1 and (i, j) not in visited:
                     union(i, j)
-
-        return count
+        return total
