9277791115e74759c32e7f70cff22fdc8b891ff8
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-19 21:17:12 +0100

    Time: 31 ms (83.20%) | Memory: 33.6 MB (53.54%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -2,20 +2,20 @@ class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         if len(connections) < n - 1:
             return -1
-        isolated = n
         root = [i for i in range(n)]
-        def find(connection):
-            if root[connection] == connection:
-                return connection
-            root[connection] = find(root[connection])
-            return root[connection]
-        def union(end1, end2):
+        isolated = n
+        def find(i):
+            if root[i] == i:
+                return i
+            root[i] = find(root[i])
+            return root[i]
+        def union(i, j):
             nonlocal isolated
-            root1 = find(end1)
-            root2 = find(end2)
+            root1 = find(i)
+            root2 = find(j)
             if root1 != root2:
                 isolated -= 1
                 root[root1] = root2
-        for u, v in connections:
-            union(u, v)
+        for end1, end2 in connections:
+            union(end1, end2)
         return isolated - 1
