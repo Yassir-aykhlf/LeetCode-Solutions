eab8697b9418eb759641e372d612d675d65f0d46
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-20 11:01:47 +0100

    Time: 123 ms (53.61%) | Memory: 20.1 MB (89.30%) - LeetSync

--- 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.py
+++ 1117-as-far-from-land-as-possible/as-far-from-land-as-possible.py
@@ -1,29 +1,27 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-        distance = 0
-        land = 0
+        distance, water = 0, 0
+        n = len(grid)
         dq = deque()
-        ROW, COL = len(grid), len(grid[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for row in range(ROW):
-            for col in range(COL):
-                if grid[row][col] == 1:
-                    dq.append((row, col))
+        for r in range(n):
+            for c in range(n):
+                if grid[r][c] == 1:
+                    dq.append((r, c))
                 else:
-                    land += 1
-        if len(dq) == ROW * COL or len(dq) == 0:
+                    water += 1
+        if len(dq) == n * n or not len(dq):
             return -1
-        while dq and land:
+        while dq and water:
             level_size = len(dq)
-            for i in range(level_size):
-                lr, lc = dq.popleft()
+            for _ in range(level_size):
+                row, col = dq.popleft()
                 for dr, dc in directions:
-                    nr, nc = lr + dr, lc + dc
-                    if 0 <= nr < ROW and \
-                        0 <= nc < COL and \
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < n and 0 <= nc < n and \
                         grid[nr][nc] == 0:
                         grid[nr][nc] = 1
+                        water -= 1
                         dq.append((nr, nc))
-                        land -= 1
             distance += 1
         return distance
