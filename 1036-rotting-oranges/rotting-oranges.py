a529a5b9bf14bb844e744cd9ba61c65b9cc0a505
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-20 10:40:16 +0100

    Time: 3 ms (75.73%) | Memory: 19.4 MB (47.17%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,23 +1,22 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh, minutes = 0, 0
         ROW, COL = len(grid), len(grid[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        fresh = 0
+        minutes = 0
         dq = deque()
-        
-        for row in range(ROW):
-            for col in range(COL):
-                if grid[row][col] == 1:
+        for r in range(ROW):
+            for c in range(COL):
+                if grid[r][c] == 1:
                     fresh += 1
-                elif grid[row][col] == 2:
-                    dq.append((row, col))
-
+                elif grid[r][c] == 2:
+                    dq.append((r, c))
         while dq and fresh:
             level_size = len(dq)
-            for i in range(level_size):
-                rr, rc = dq.popleft()
+            for _ in range(level_size):
+                row, col = dq.popleft()
                 for dr, dc in directions:
-                    nr, nc = rr + dr, rc + dc
+                    nr, nc = row + dr, col + dc
                     if 0 <= nr < ROW and \
                         0 <= nc < COL and \
                         grid[nr][nc] == 1:
@@ -25,5 +24,4 @@ class Solution:
                         fresh -= 1
                         dq.append((nr, nc))
             minutes += 1
-        
         return minutes if not fresh else -1
