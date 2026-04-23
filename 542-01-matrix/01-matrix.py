9784903279fab5ced3a55748c7553169e10fe70f
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-23 17:08:53 +0100

    Time: 145 ms (34.66%) | Memory: 22 MB (65.36%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,23 +1,23 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        dq = deque()
         ROW, COL = len(mat), len(mat[0])
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        dq = deque()
+        visited = set()
         for row in range(ROW):
             for col in range(COL):
-                if mat[row][col] == 1:
-                    mat[row][col] = -1
-                else:
+                if mat[row][col] == 0:
                     dq.append((row, col))
+                else:
+                    mat[row][col] = -1
         while dq:
-            level_size = len(dq)
-            for i in range(level_size):
-                r, c = dq.popleft()
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < ROW and \
-                        0 <= nc < COL and \
-                        mat[nr][nc] == -1:
-                        mat[nr][nc] = mat[r][c] + 1
-                        dq.append((nr, nc))
+            row, col = dq.popleft()
+            for dr, dc in directions:
+                nr, nc = row + dr, col + dc
+                if 0 <= nr < ROW and 0 <= nc < COL and \
+                    (nr, nc) not in visited and \
+                    mat[nr][nc] != 0:
+                    visited.add((nr, nc))
+                    dq.append((nr, nc))
+                    mat[nr][nc] = mat[row][col] + 1
         return mat
