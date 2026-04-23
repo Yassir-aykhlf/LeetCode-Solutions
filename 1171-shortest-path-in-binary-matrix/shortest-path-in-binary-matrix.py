75e37d591fc531c51d68cbc771a791958f5fa69e
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-23 16:58:24 +0100

    Time: 166 ms (55.85%) | Memory: 20.9 MB (16.50%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -8,17 +8,20 @@ class Solution:
             ( 0, -1),          ( 0, 1),
             ( 1, -1), ( 1, 0), ( 1, 1)
         ]
-        dq = deque([(0, 0, 1)])
-        grid[0][0] = 1
+        distance = 1
+        dq = deque([(0, 0)])
+        visited = {(0, 0)}
         while dq:
-            r, c, distance = dq.popleft()
-            if r == n - 1 and c == n - 1:
-                return distance
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < n and \
-                    0 <= nc < n and \
-                    grid[nr][nc] == 0:
-                    grid[nr][nc] = 1
-                    dq.append((nr, nc, distance + 1))
+            level_size = len(dq)
+            for _ in range(level_size):
+                row, col = dq.popleft()
+                if row == n-1 and col == n-1 and grid[row][col] == 0:
+                    return distance
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < n and 0 <= nc < n and \
+                        grid[nr][nc] == 0 and (nr, nc) not in visited:
+                        visited.add((nr, nc))
+                        dq.append((nr, nc))
+            distance += 1
         return -1
