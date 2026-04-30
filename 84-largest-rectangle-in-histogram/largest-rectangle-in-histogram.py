24e9c0149cb4dc7f3401a8bb9462a5054d60037c
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-04-30 15:28:47 +0100

    Time: 130 ms (36.99%) | Memory: 37.9 MB (7.52%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -8,7 +8,7 @@ class Solution:
                 idx, hei = stack.pop()
                 max_area = max(max_area, hei * (i - idx))
                 start = idx
-            stack.append((start, h))
+            stack.append([start, h])
         for i, h in stack:
-            max_area = max(max_area, h * (len(heights) - i))
+            max_area = max(max_area, (len(heights) - i) * h)
         return max_area
