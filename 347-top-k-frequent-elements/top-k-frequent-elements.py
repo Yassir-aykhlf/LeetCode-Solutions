39b9478115ddd97946c695cefe04b5f2ce30fe27
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-05-09 16:30:31 +0100

    Time: 0 ms (100.00%) | Memory: 22.9 MB (42.79%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,4 +1,4 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = collections.Counter(nums)
-        return heapq.nlargest(k, freq.keys(), key=lambda x: freq[x])
+        freq = Counter(nums)
+        return heapq.nlargest(k, freq.keys(), key=lambda el: freq[el])
