b02b80f28cbfc609b7c07bc0fbb1e0ec76fa3c2c
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-05-12 20:06:04 +0100

    Time: 4 ms (28.71%) | Memory: 19.4 MB (45.63%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -1,19 +1,17 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
-        def check(sum):
-            # can make at least k subarrays
-            curr_sum = 0
-            splits = 1
-            for r in range(len(nums)):
-                if curr_sum + nums[r] > sum:
-                    splits += 1
-                    curr_sum = nums[r]
-                else:
-                    curr_sum += nums[r]
-            return splits <= k
-        
         lo, hi = max(nums), sum(nums)
         ans = hi
+        def check(sum_):
+            splits = 1
+            curr_sum = 0
+            for n in nums:
+                if curr_sum + n > sum_:
+                    curr_sum = n
+                    splits += 1
+                else:
+                    curr_sum += n
+            return splits <= k
         while lo <= hi:
             mid = (lo + hi) // 2
             if check(mid):
