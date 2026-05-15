95feaaa9000bf66b44da4ec8c07629579c966ae8
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-05-15 18:39:51 +0100

    Time: 0 ms (100.00%) | Memory: 19.2 MB (94.54%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -15,5 +15,5 @@ class Solution:
                 curr.next = list2
                 list2 = list2.next
             curr = curr.next
-        curr.next = list1 or list2
+        curr.next = list1 if list1 else list2
         return dummy.next
