903572486557b605af2811671a6fbfffa0fcb471
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-05-20 21:04:26 +0100

    Time: 175 ms (42.36%) | Memory: 19.4 MB (94.73%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        freq = collections.defaultdict(int)
+        count = defaultdict(int)
         max_len = 0
+        max_count = 0
         l = 0
         for r in range(len(answerKey)):
-            freq[answerKey[r]] += 1
-            while r - l + 1 - max(freq.values()) > k:
-                freq[answerKey[l]] -= 1
+            count[answerKey[r]] += 1
+            max_count = max(max_count, count[answerKey[r]])
+            while (r - l + 1) - max_count > k:
+                count[answerKey[l]] -= 1
                 l += 1
             max_len = max(max_len, r - l + 1)
         return max_len
