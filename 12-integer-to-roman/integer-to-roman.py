adb8b1d6b14b80640f92246ef524a8ce44db942b
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-06-09 14:15:42 +0100

    Time: 12 ms (9.63%) | Memory: 19.4 MB (26.49%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman_map = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+        ]
+        res = []
+        for val, sym in roman_map:
+            m = num // val
+            res += m * [sym]
+            num -= m * val
+        return "".join(res)
