1d9e815799e323fb53c65d8028c5e9a07d3a8daf
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-06-13 14:38:05 +0100

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.22%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -1,12 +1,24 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        nums1_h = {}
-        for id, val in nums1:
-            nums1_h[id] = val
-        nums2_h = {**nums1_h}
-        for id, val in nums2:
-            if id in nums1_h:
-                nums2_h[id] = nums1_h[id] + val
+        res = []
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            id1, val1 = nums1[i]
+            id2, val2 = nums2[j]
+            if id1 == id2:
+                res.append([id1, val1 + val2])
+                i += 1
+                j += 1
+            elif id1 < id2:
+                res.append([id1, val1])
+                i += 1
             else:
-                nums2_h[id] = val
-        return sorted([[id, val] for id, val in nums2_h.items()])
+                res.append([id2, val2])
+                j += 1
+        while i < len(nums1):
+            res.append(nums1[i])
+            i += 1
+        while j < len(nums2):
+            res.append(nums2[j])
+            j += 1
+        return res
