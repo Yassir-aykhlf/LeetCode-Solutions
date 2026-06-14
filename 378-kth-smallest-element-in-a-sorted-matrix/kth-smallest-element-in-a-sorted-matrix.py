e941bf7643ced63bc59cabd16aece15d1991e8ce
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-06-14 13:26:13 +0100

    Time: 0 ms (100.00%) | Memory: 22.8 MB (90.28%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,28 +1,18 @@
-"""
-Given an n x n matrix where each of the rows and columns is sorted in ascending order, return the kth smallest element in the matrix.
-
-Note that it is the kth smallest element in the sorted order, not the kth distinct element.
-
-You must find a solution with a memory complexity better than O(n^2).
-"""
-
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        return heapq.nsmallest(k, [num for arr in matrix for num in arr])[-1]
-        # n = len(matrix)
-        # lo, hi = matrix[0][0], matrix[n - 1][n - 1]
-        # while lo < hi:
-        #     mid = (lo + hi) // 2
-        #     count = 0
-        #     row, col = 0, n - 1
-        #     while row < n and col >= 0:
-        #         if matrix[row][col] <= mid:
-        #             count += col + 1
-        #             row += 1
-        #         else:
-        #             col -= 1
-        #     if count >= k:
-        #         hi = mid
-        #     else:
-        #         lo = mid + 1
-        # return hi
+        n = len(matrix)
+        l, r = matrix[0][0], matrix[n - 1][n - 1]
+        while l <= r:
+            mid = (l + r) // 2
+            count, row, col = 0, 0, n - 1
+            while row < n and col >= 0:
+                if matrix[row][col] <= mid:
+                    count += col + 1
+                    row += 1
+                else:
+                    col -= 1
+            if count < k:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l
