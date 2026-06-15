bfcb8fc8315d1fe488cc67e9465d0ddcf8b0acf0
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-06-15 09:29:57 +0100

    Time: 425 ms (28.19%) | Memory: 29.9 MB (93.97%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -1,29 +1,12 @@
-# class Solution:
-#     def maximumCandies(self, candies: List[int], k: int) -> int:
-#         l, r = 0, max(candies)
-#         def check(max_per_child):
-#             count = 0
-#             for c in candies:
-#                 if c >= max_per_child:
-#                     count += c // max_per_child
-#             return count >= k
-#         while l < r:
-#             mid = (l + r) // 2
-#             if check(mid):
-#                 l = mid
-#             else:
-#                 r = mid - 1
-#         return l
-
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
         l, r = 1, max(candies)
-        def check(max_per_child):
-            count = 0
+        def check(c_per_k):
+            served = 0
             for c in candies:
-                if c >= max_per_child:
-                    count += c // max_per_child
-            return count >= k
+                if c >= c_per_k:
+                    served += c // c_per_k
+            return served >= k
         ans = 0
         while l <= r:
             mid = (l + r) // 2
