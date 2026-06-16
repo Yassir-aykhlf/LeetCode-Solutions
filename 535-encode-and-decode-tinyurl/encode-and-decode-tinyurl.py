e5a1923e0056cbfbf6ad8624465961f4602816f2
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-06-16 11:31:18 +0100

    Time: 51 ms (30.58%) | Memory: 22.6 MB (7.48%) - LeetSync

--- 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.py
@@ -1,9 +1,11 @@
 import hashlib
 
 class Codec:
-    mapping = {}
+    def __init__(self):
+        self.mapping = {}
+
     def encode(self, longUrl: str) -> str:
-        code = hashlib.sha256().hexdigest()[:6]
+        code = hashlib.sha256(longUrl.encode()).hexdigest()[:6]
         self.mapping[code] = longUrl
         return f"http://tinyurl.com/{code}"
 
@@ -11,7 +13,3 @@ class Codec:
         code = shortUrl[-6:]
         if code in self.mapping:
             return self.mapping[code]
-
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.decode(codec.encode(url))
