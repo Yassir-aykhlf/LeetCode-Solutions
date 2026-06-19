f1d8f71e8b082824d8d17e2d910a222b2aeba33d
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-06-19 09:49:34 +0100

    Time: 0 ms (100.00%) | Memory: 19.2 MB (51.43%) - LeetSync

--- 1999-longer-contiguous-segments-of-ones-than-zeros/longer-contiguous-segments-of-ones-than-zeros.py
+++ 1999-longer-contiguous-segments-of-ones-than-zeros/longer-contiguous-segments-of-ones-than-zeros.py
@@ -6,13 +6,11 @@ class Solution:
             if s[r] == '0':
                 z_len += 1
             else:
-                max_z = max(max_z, z_len)
                 z_len = 0
             if s[r] == '1':
                 o_len += 1
             else:
-                max_o = max(max_o, o_len)
                 o_len = 0
-        max_z = max(max_z, z_len)
-        max_o = max(max_o, o_len)
+            max_z = max(max_z, z_len)
+            max_o = max(max_o, o_len)
         return max_o > max_z
