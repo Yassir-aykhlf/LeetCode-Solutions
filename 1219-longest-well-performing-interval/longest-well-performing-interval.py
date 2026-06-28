18c1adf3acd698834b09d35f60af988dd4924e45
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-06-28 14:06:51 +0100

    Time: 20 ms (54.91%) | Memory: 19.9 MB (42.20%) - LeetSync

--- 1219-longest-well-performing-interval/longest-well-performing-interval.py
+++ 1219-longest-well-performing-interval/longest-well-performing-interval.py
@@ -1,14 +1,15 @@
 class Solution:
     def longestWPI(self, hours: List[int]) -> int:
-        acc_index = {0: -1}
-        longest_window = 0
-        acc = 0
+        seen_scores = {}
+        curr_score = 0
+        max_len = 0
         for i, h in enumerate(hours):
-            acc += 1 if h > 8 else -1
-            if acc > 0:
-                longest_window = i + 1
-            elif acc - 1 in acc_index:
-                longest_window = max(longest_window, i - acc_index[acc - 1])
-            if acc not in acc_index:
-                acc_index[acc] = i
-        return longest_window
+            curr_score += 1 if h > 8 else -1
+            if curr_score > 0:
+                max_len = i + 1
+            else:
+                if (curr_score - 1) in seen_scores:
+                    max_len = max(max_len, i - seen_scores[curr_score - 1])
+            if curr_score not in seen_scores:
+                seen_scores[curr_score] = i
+        return max_len
