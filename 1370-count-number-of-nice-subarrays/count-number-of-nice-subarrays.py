97eb59d45bbf2a6cfb8f6fbca8430caaa65bd10f
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-06-29 13:01:47 +0100

    Time: 98 ms (70.56%) | Memory: 25.9 MB (36.63%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,12 +1,13 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        sum_count = {0: 1}
-        curr_sum = 0
         count = 0
-        for n in nums:
-            curr_sum += 1 if n % 2 else 0
-            target = curr_sum - k
-            if target in sum_count:
-                count += sum_count[target]
-            sum_count[curr_sum] = sum_count.get(curr_sum, 0) + 1
+        curr_sum = 0
+        sum_to_count = {0:1}
+        for num in nums:
+            if num % 2:
+                curr_sum += 1
+            prev_sum = curr_sum - k
+            if prev_sum in sum_to_count:
+                count += sum_to_count[prev_sum]
+            sum_to_count[curr_sum] = sum_to_count.get(curr_sum, 0) + 1
         return count
