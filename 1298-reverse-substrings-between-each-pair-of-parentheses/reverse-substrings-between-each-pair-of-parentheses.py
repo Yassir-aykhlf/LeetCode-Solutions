1e4391f77885a5657ba1a1e4d02f0a891ca89746
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-06-30 06:50:58 -0400

    Time: 1 ms (70.11%) | Memory: 19.4 MB (26.29%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
@@ -3,12 +3,12 @@ class Solution:
         stack = []
         curr = []
         for c in s:
-            if c == '(':
+            if c.isalpha():
+                curr.append(c)
+            elif c == '(':
                 stack.append(curr)
                 curr = []
             elif c == ')':
                 curr.reverse()
                 curr = stack.pop() + curr
-            else:
-                curr.append(c)
         return ''.join(curr)
