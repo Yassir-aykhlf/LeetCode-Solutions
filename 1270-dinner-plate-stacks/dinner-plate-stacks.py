9c2808b227c99264a8e11d76666bd67f72bf6eac
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-07-01 06:35:43 -0400

    Time: 200 ms (61.54%) | Memory: 85.7 MB (99.23%) - LeetSync

--- 1270-dinner-plate-stacks/dinner-plate-stacks.py
+++ 1270-dinner-plate-stacks/dinner-plate-stacks.py
@@ -1,38 +1,46 @@
-import heapq
-
 class DinnerPlates:
+
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.stacks = []
-        self.available = []
+        self.stack = []
+        self.leftmost = []
 
     def push(self, val: int) -> None:
-        while self.available and self.available[0] >= len(self.stacks):
-            heapq.heappop(self.available)
-        if self.available:
-            idx = self.available[0]
-            self.stacks[idx].append(val)
-            if len(self.stacks[idx]) == self.capacity:
-                heapq.heappop(self.available)
+        while self.stack and not self.stack[-1]:
+            self.stack.pop()
+        while self.leftmost and self.leftmost[0] >= len(self.stack):
+            heapq.heappop(self.leftmost)
+        if self.leftmost:
+            idx = self.leftmost[0]
+            self.stack[idx].append(val)
+            if self.capacity == len(self.stack[idx]):
+                heapq.heappop(self.leftmost)
         else:
-            self.stacks.append([val])
-            if self.capacity > 1:
-                heapq.heappush(self.available, len(self.stacks) - 1)
+            if not self.stack or len(self.stack[-1]) == self.capacity:
+                self.stack.append([val])
+            else:
+                self.stack[-1].append(val)
 
     def pop(self) -> int:
-        while self.stacks and not self.stacks[-1]:
-            self.stacks.pop()
-        if not self.stacks:
-            return -1
-        val = self.stacks[-1].pop()
-        if len(self.stacks[-1]) == self.capacity - 1:
-            heapq.heappush(self.available, len(self.stacks) - 1)
-        return val
+        while self.stack and not self.stack[-1]:
+            self.stack.pop()
+        if self.stack and self.stack[-1]:
+            val = self.stack[-1].pop()
+            return val
+        return -1
 
     def popAtStack(self, index: int) -> int:
-        if not self.stacks or index < 0 or index >= len(self.stacks) or not self.stacks[index]:
+        while self.stack and not self.stack[-1]:
+            self.stack.pop()
+        if index >= len(self.stack) or len(self.stack[index]) == 0:
             return -1
-        val = self.stacks[index].pop()
-        if len(self.stacks[index]) == self.capacity - 1:
-            heapq.heappush(self.available, index)
+        val = self.stack[index].pop()
+        if len(self.stack[index]) == self.capacity - 1:
+            heapq.heappush(self.leftmost, index)
         return val
+
+# Your DinnerPlates object will be instantiated and called as such:
+# obj = DinnerPlates(capacity)
+# obj.push(val)
+# param_2 = obj.pop()
+# param_3 = obj.popAtStack(index)
