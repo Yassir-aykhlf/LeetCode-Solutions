4e32483de664ff548255a4c2d92a3245ef02bcfa
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-07-11 13:20:37 -0400

    Time: 460 ms (38.36%) | Memory: 19.3 MB (33.84%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -3,7 +3,7 @@ class Solution:
         nums.sort()
         n = len(nums)
         count = 0
-        for i in range(n - 1, - 1, -1):
+        for i in range(n - 1, -1, -1):
             l = 0
             r = i - 1
             while l < r:
