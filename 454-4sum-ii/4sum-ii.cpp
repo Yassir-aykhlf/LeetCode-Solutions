416b99897741d354a5ee7ad3bb5cfc56b53fdffd
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-07-24 11:44:39 -0400

    Time: 94 ms (68.29%) | Memory: 28.3 MB (40.73%) - LeetSync

--- 454-4sum-ii/4sum-ii.cpp
+++ 454-4sum-ii/4sum-ii.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         std::unordered_map<int, int> map;
-        for (size_t i = 0; i < nums1.size(); ++i) {
-            for (size_t j = 0; j < nums2.size(); ++j) {
-                map[nums1[i] + nums2[j]] += 1;
+        int count = 0;
+        for (int num1 : nums1) {
+            for (int num2 : nums2) {
+                map[num1 + num2] += 1; 
             }
         }
-        int count = 0;
-        for (size_t i = 0; i < nums3.size(); ++i) {
-            for (size_t j = 0; j < nums4.size(); ++j) {
-                count += map[-(nums3[i] + nums4[j])];
+        for (int num3 : nums3) {
+            for (int num4: nums4) {
+                count += map[-(num3 + num4)];
             }
         }
         return count;
