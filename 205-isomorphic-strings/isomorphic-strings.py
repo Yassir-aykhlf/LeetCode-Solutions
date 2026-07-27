0bb15c1faeef75a4026aaa3664708121b9a01d6e
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-07-27 04:56:20 -0400

    Time: 7 ms (36.64%) | Memory: 19.1 MB (97.48%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -2,10 +2,10 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         s_t = {}
         t_s = {}
-        for s_, t_ in zip(s, t):
-            if s_ in s_t and s_t[s_] != t_ or \
-                t_ in t_s and t_s[t_] != s_:
+        for _s, _t in zip(s, t):
+            if _s in s_t and s_t[_s] != _t or \
+                _t in t_s and t_s[_t] != _s:
                 return False
-            s_t[s_] = t_
-            t_s[t_] = s_
+            s_t[_s] = _t
+            t_s[_t] = _s
         return True
