d0911bd353676edf392e6bdb3e92c669e8354efb
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-07-30 10:19:47 -0400

    Time: 79 ms (53.84%) | Memory: 25.5 MB (8.00%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,14 +1,12 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        max_len = 0
-        # when was the last position we have seen a sum equal to the current sum
-        # cuz that means everything in between sums to zero
-        sum_to_pos = {0: -1}
+        state = {0: -1}
         curr_sum = 0
-        for i, num in enumerate(nums):
-            curr_sum += 1 if num == 1 else -1
-            if curr_sum in sum_to_pos:
-                max_len = max(max_len, i - sum_to_pos[curr_sum])
+        res = 0
+        for i in range(len(nums)):
+            curr_sum += 1 if nums[i] else -1
+            if curr_sum in state:
+                res = max(res, i - state[curr_sum])
             else:
-                sum_to_pos[curr_sum] = i
-        return max_len
+                state[curr_sum] = i
+        return res
