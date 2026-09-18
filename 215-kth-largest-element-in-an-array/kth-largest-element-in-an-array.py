5e18e29bc8011a216d3ccb524b203f78954280e9
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-09-18 10:50:10 +0100

    Time: 151 ms (7.83%) | Memory: 35 MB (5.07%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,21 +1,3 @@
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
         return heapq.nlargest(k, nums)[-1]
-        # n = len(nums)
-        # target_index = n - k
-        # l = 0
-        # r = n - 1
-        # while l <= r:
-        #     pivot = nums[r]
-        #     cursor = l
-        #     for i in range(l, r):
-        #         if nums[i] <= pivot:
-        #             nums[cursor], nums[i] = nums[i], nums[cursor]
-        #             cursor += 1
-        #     nums[cursor], nums[r] = nums[r], nums[cursor]
-        #     if cursor == target_index:
-        #         return nums[cursor]
-        #     elif cursor < target_index:
-        #         l = cursor + 1
-        #     else:
-        #         r = cursor - 1
