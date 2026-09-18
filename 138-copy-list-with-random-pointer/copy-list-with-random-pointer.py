3eb19bf8655cf426b5acafd9c4c7a37a5493ba7b
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-09-18 10:39:29 +0100

    Time: 42 ms (86.86%) | Memory: 19.9 MB (88.75%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -9,16 +9,14 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        dc = {None: None}
-        c = head
-        while c:
-            n = Node(c.val)
-            dc[c] = n
-            c = c.next
-        c = head
-        while c:
-            dc[c].next = dc[c.next]
-            dc[c].random = dc[c.random]
-            c = c.next
-        return dc[head]
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
+        mapping = {None: None}
+        curr = head
+        while curr:
+            mapping[curr] = Node(curr.val)
+            curr = curr.next
+        curr = head
+        while curr:
+            mapping[curr].next = mapping[curr.next]
+            mapping[curr].random = mapping[curr.random]
+            curr = curr.next
+        return mapping[head]
