f7e8e09ad7a3b9893c2af8f5584aa569fa204590
yassir aykhlf <aykhlf15yassir@gmail.com>
2026-09-25 20:00:41 +0100

    Time: 34 ms (23.56%) | Memory: 23.3 MB (38.58%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,11 +1,11 @@
 class Solution:
-    def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        count = 0
+    def numPairsDivisibleBy60(self, time: list[int]) -> int:
         rems = {}
+        count = 0
         for t in time:
-            rem = t % 60
-            tar = (60 - rem) % 60
-            if tar in rems:
-                count += rems[tar]
+            rem = (t % 60) % 60
+            target = (60 - rem) % 60
+            if target in rems:
+                count += rems[target]
             rems[rem] = rems.get(rem, 0) + 1
         return count
